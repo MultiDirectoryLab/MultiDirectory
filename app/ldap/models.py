ec7fbe6cfbba20bf4b7149f625478d59aa28c92f
Mastermind-U <rex49513@gmail.com>
2022-12-06 10:55:00 +0300

    added: main database schema

---
+++ app/ldap/models.py
@@ -0,0 +1,111 @@
+"""MultiDirectory LDAP models."""
+
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    UniqueConstraint,
+    func,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    declarative_base,
+    declarative_mixin,
+    declared_attr,
+    relationship,
+)
+
+Base = declarative_base()
+
+
+class CatalogueSetting(Base):
+    """Catalogue params unit."""
+
+    __tablename__ = "Settings"
+
+    id = Column('id', Integer, primary_key=True)  # noqa: A003
+    name = Column(String, nullable=False, index=True)
+    value = Column(String, nullable=False)
+
+
+class Directory(Base):
+    """Chierarcy of catalogue unit."""
+
+    __tablename__ = "Directories"
+
+    id = Column('id', Integer, primary_key=True)  # noqa: A003
+
+    parent_id = Column('parentId', Integer, index=True, nullable=True)
+    parent: list['Directory'] = relationship(
+        lambda: Directory, remote_side=id, backref='sub_directories')
+    object_class = Column('objectClass', String, nullable=False)
+
+    name = Column(String, nullable=False)
+
+    created_at = Column(
+        'whenCreated',
+        DateTime(timezone=True),
+        server_default=func.now(), nullable=False)
+    updated_at = Column(
+        'whenChanged',
+        DateTime(timezone=True),
+        onupdate=func.now(), nullable=False)
+
+    users: list['User'] = relationship("User")
+
+    __table_args__ = (
+        UniqueConstraint(
+            'customer_id',
+            'location_code',
+            name='_customer_location_uc',
+        ),
+    )
+
+
+@declarative_mixin
+class DirectoryReferenceMixin:
+    """Mixin with dir id reference."""
+
+    id = Column('id', primary_key=True)  # noqa: A003
+
+    @declared_attr
+    def directory_id(cls) -> Mapped[Directory]:  # noqa: N805, D102
+        return Column(
+            'directoryId', ForeignKey('Directories.id'), nullable=False)
+
+
+class User(DirectoryReferenceMixin):
+    """Users data."""
+
+    __tablename__ = "Users"
+
+    sam_accout_name = Column(
+        'sAMAccountName', String, nullable=False, unique=True)
+    user_principal_name = Column(
+        'userPrincipalName', String, nullable=False, unique=True)
+
+    display_name = Column('displayName', String, nullable=True)
+    password = Column(String, nullable=True)
+
+
+class Group(DirectoryReferenceMixin):
+    """Group params."""
+
+    __tablename__ = "Groups"
+
+
+class Computer(DirectoryReferenceMixin):
+    """Computers data."""
+
+    __tablename__ = "Computers"
+
+
+class Attrubute(DirectoryReferenceMixin):
+    """Attributes data."""
+
+    __tablename__ = "Attributes"
+
+    name = Column(String, nullable=False)
+    value = Column(String, nullable=False)
