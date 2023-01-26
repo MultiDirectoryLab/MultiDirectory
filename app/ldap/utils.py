0996eea62d4e3d2404386ea4196b4ccd0870332f
Mastermind-U <rex49513@gmail.com>
2023-01-26 14:18:20 +0300

    removed: cast_filter2sql

--- app/ldap/utils.py
+++ app/ldap/utils.py
@@ -1,14 +1,11 @@
 """Utils module for different functions."""
-from functools import cache
-
-from ldap_filter import Filter
-from sqlalchemy import and_, or_, select
+from sqlalchemy import select
 
 from models.database import async_session
-from models.ldap3 import Attrubute, CatalogueSetting, User
+from models.ldap3 import CatalogueSetting
 
 
-# @cache
+# TODO: Add caching
 async def get_base_dn() -> str:
     """Get base dn for e.g. DC=multifactor,DC=dev.
 
@@ -22,40 +19,3 @@ async def get_base_dn() -> str:
         return ','.join((
             f'DC={value}' for value in
             cat_result.scalar_one().value.split('.')))
-
-
-def cast_filter2sql(root_q: str):
-    op_map = {
-        '!': 'ne',
-        '=': 'eq',
-        '>=': 'ge',
-        '<=': 'le',
-    }
-
-    def cast(expr: Filter):
-        conditions = []
-        for item in expr.filters:
-            if item.comp not in '&|!':
-                attr = item.attr.lower()
-                if attr in User.attrs:
-                    col = getattr(User, attr)
-                    conditions.append(
-                        getattr(col, op_map[item.comp])(
-                            attr, item.val.lower()))
-                else:
-                    conditions.append(and_(
-                        Attrubute.name.ilike(attr),
-                        Attrubute.value.ilike(item.val),
-                    ))
-            else:
-                conditions.append(cast(item))
-
-
-        if expr.comp == '&':
-            return and_(*conditions)
-        elif expr.comp == '|':
-            return or_(*conditions)
-        else:
-            raise TypeError
-
-    return cast(Filter.parse(root_q))
