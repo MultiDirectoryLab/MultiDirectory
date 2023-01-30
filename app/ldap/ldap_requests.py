f9e54f5963475c5cb950cb2db4f52437fa351139
Mastermind-U <rex49513@gmail.com>
2023-01-30 15:23:15 +0300

    refactor: added filter interpreter module

--- app/ldap/ldap_requests.py
+++ app/ldap/ldap_requests.py
@@ -6,7 +6,6 @@ from collections import defaultdict
 from typing import AsyncGenerator, ClassVar
 
 from pydantic import BaseModel, Field, validator
-from sqlalchemy import and_, func, not_, or_
 from sqlalchemy.future import select
 from sqlalchemy.orm import joinedload, lazyload
 
@@ -16,6 +15,7 @@ from models.ldap3 import Attribute, CatalogueSetting, Directory, Path, User
 
 from .asn1parser import ASN1Row
 from .dialogue import LDAPCodes, Session
+from .filter_interpreter import cast_filter2sql
 from .ldap_responses import (
     BaseResponse,
     BindResponse,
@@ -253,75 +253,6 @@ class SearchRequest(BaseRequest):
             return None
         return v
 
-    def cast_filter2sql(self):
-        """Recursively cast Filter to SQLAlchemy conditions."""
-        from operator import eq, ge, le
-        op_map = {
-            3: eq,
-            5: ge,
-            6: le,
-        }
-
-        def get_substring(right: ASN1Row) -> str:  # RFC 4511
-            expr = right.value[0]
-            value = expr.value
-            index = expr.tag_id.value
-            return [f"{value}%", f"%{value}%", f"%{value}"][index]
-
-        def from_filter(model: type, item, attr, right):
-            is_substring = item.tag_id.value == 4
-            col = getattr(model, attr)
-
-            if is_substring:
-                return col.ilike(get_substring(right))
-            op_method = op_map[item.tag_id.value]
-            return op_method(func.lower(col), right.value.lower())
-
-        def cast_item(item):
-            if item.tag_id.value == 7:
-                attr = item.value.lower()
-
-                if attr in User.search_fields:
-                    return not_(eq(getattr(User, attr), None))
-
-                if attr in Directory.search_fields:
-                    return not_(eq(getattr(Directory, attr), None))
-
-                return func.lower(Attribute.value) == item.value.lower()
-
-            left, right = item.value
-            attr = left.value.lower()
-            is_substring = item.tag_id.value == 4
-
-            if attr in User.search_fields:
-                return from_filter(User, item, attr, right)
-            elif attr in Directory.search_fields:
-                return from_filter(Directory, item, attr, right)
-
-            else:
-                if is_substring:
-                    cond = Attribute.value.ilike(get_substring(right))
-                else:
-                    cond = func.lower(
-                        Attribute.value) == right.value.lower()
-
-                return and_(func.lower(Attribute.name) == attr, cond)
-
-        def cast(expr):
-            conditions = []
-            if expr.tag_id.value in range(3):
-                for item in expr.value:
-                    if item.tag_id.value in range(3):  # &|!
-                        conditions.append(cast(item))
-                        continue
-
-                    conditions.append(cast_item(item))
-
-                return [and_, or_, not_][expr.tag_id.value](*conditions)
-
-            return cast_item(expr)
-        return cast(self.filter)
-
     @staticmethod
     async def get_root_dse(attributes: list[str])\
             -> defaultdict[str, list[str]]:
@@ -401,7 +332,7 @@ class SearchRequest(BaseRequest):
 
     async def whole_subtree_view(self):
         """Yield subtree result."""
-        condition = self.cast_filter2sql()
+        condition = cast_filter2sql(self.filter)
 
         query = select(Directory)\
             .join(User, isouter=True)\
