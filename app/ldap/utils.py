fa812809052d35a86540ee77c2cbfcc4e0eeeae3
Mastermind-U <rex49513@gmail.com>
2023-02-09 14:52:16 +0300

    added: domain and generalized time utils

--- app/ldap/utils.py
+++ app/ldap/utils.py
@@ -1,4 +1,7 @@
 """Utils module for different functions."""
+from datetime import datetime
+
+import pytz
 from asyncio_cache import cache
 from sqlalchemy import select
 
@@ -20,3 +23,20 @@ async def get_base_dn() -> str:
         return ','.join((
             f'DC={value}' for value in
             cat_result.scalar_one().value.split('.')))
+
+
+@cache
+async def get_domain() -> str:
+    """Get domain name in normal form (multifactor.dev)."""
+    async with async_session() as session:
+        cat_result = await session.execute(
+            select(CatalogueSetting)
+            .filter(CatalogueSetting.name == 'defaultNamingContext'),
+        )
+        return cat_result.scalar_one().value
+
+
+def get_generalized_now():
+    """Get generalized time (formated) with tz."""
+    return datetime.now(  # NOTE: possible setting
+        pytz.timezone('Europe/Moscow')).strftime('%Y%m%d%H%M%S.%f%z')
