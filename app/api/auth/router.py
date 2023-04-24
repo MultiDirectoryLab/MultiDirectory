47d914b73cecf751bd941e51351d5645b9c2ef2e
Mastermind-U <rex49513@gmail.com>
2023-04-24 15:09:17 +0300

    added: working auth api

--- app/api/auth/router.py
+++ app/api/auth/router.py
@@ -1,30 +1,32 @@
+"""Auth api."""
+
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from config import Settings, get_settings
-from models.database import AsyncSession
+from models.database import get_session
 
 from .oauth2 import authenticate_user, create_token, get_current_user
-from .schema import Token, UserModel
+from .schema import Login, Token, UserModel
 
-auth_router = APIRouter()
+auth_router = APIRouter(prefix='/auth')
 
 
-@auth_router.post("/token/get", response_model=Token)
+@auth_router.post("/token/get")
 async def login_for_access_token(
-    form: OAuth2PasswordRequestForm = Depends(),
+    form: Login,
+    session: AsyncSession = Depends(get_session),
     settings: Settings = Depends(get_settings),
-    session: AsyncSession = Depends(),
-):
+) -> Token:
     """Get refresh and access token with login.
 
-    :param OAuth2PasswordRequestForm form: password form, defaults to Depends()
+    :param Login form: password form, defaults to Depends()
     :param Settings settings: app settings, defaults to Depends(get_settings)
     :param AsyncSession session: sa session, defaults to Depends()
     :raises HTTPException: in invalid user
     :return Token: refresh and access token
     """
-    user = await authenticate_user(session, form.username, form.password)
+    user = await authenticate_user(session, form.name, form.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -46,17 +48,18 @@ async def login_for_access_token(
         token_type='refresh',
     )
 
-    return {
-        "access_token": access_token,
-        "refresh_token": refresh_token,
-        "token_type": "bearer"}
+    return Token(
+        access_token=access_token,
+        refresh_token=refresh_token,
+        type="bearer",
+    )
 
 
-@auth_router.post("/token/refresh", response_model=Token)
-async def get_refresh_token():
+@auth_router.post("/token/refresh")
+async def get_refresh_token(current_user: UserModel = Depends(get_current_user)) -> Token:
     pass
 
 
-@auth_router.get("/users/me/", response_model=UserModel)
-async def users_me(current_user: UserModel = Depends(get_current_user)):
+@auth_router.get("/users/me/")
+async def users_me(current_user: UserModel = Depends(get_current_user)) -> UserModel:
     return current_user
