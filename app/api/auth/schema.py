716e193d474977879ae8b2f54b0c151b216b0d6e
Mastermind-U <rex49513@gmail.com>
2023-04-24 15:08:08 +0300

    added: schema for login

--- app/api/auth/schema.py
+++ app/api/auth/schema.py
@@ -1,16 +1,29 @@
+"""Schemas for auth module."""
+
 from pydantic import BaseModel
 
 from models.ldap3 import User
 
 
+class Login(BaseModel):
+    """Login form."""
+
+    name: str
+    password: str
+
+
 class Token(BaseModel):
+    """Token model."""
+
     access_token: str
     refresh_token: str
-    token_type: str
+    type: str  # noqa: A003
 
 
 class UserModel(BaseModel):
-    id: int
+    """User model, alias for db user."""
+
+    id: int  # noqa: A003
     sam_accout_name: str
     user_principal_name: str
     mail: str
