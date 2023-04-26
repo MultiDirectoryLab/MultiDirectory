5ed72fdd5a6c381c61e470bff79fac9b8340bdb0
Mastermind-U <rex49513@gmail.com>
2023-04-26 18:34:56 +0300

    refactor: grant refresh token

--- app/api/auth/oauth2.py
+++ app/api/auth/oauth2.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Literal
 
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2AuthorizationCodeBearer
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
 from config import Settings, get_settings
@@ -16,11 +16,7 @@ from .schema import UserModel
 ALGORITHM = "HS256"
 
 
-oauth2 = OAuth2AuthorizationCodeBearer(
-    authorizationUrl="users",
-    tokenUrl="token",
-    refreshUrl="refresh",
-)
+oauth2 = OAuth2PasswordBearer(tokenUrl="auth/token/get")
 
 
 async def authenticate_user(
@@ -40,18 +36,27 @@ def create_token(
     data: dict,
     secret: str,
     expires_minutes: int,
-    token_type: Literal['refresh', 'access'],
+    grant_type: Literal['refresh', 'access'],
 ) -> str:
+    """Create jwt token.
+
+    :param dict data: data dict
+    :param str secret: secret key
+    :param int expires_minutes: exire time in minutes
+    :param Literal[refresh, access] grant_type: grant type flag
+    :return str: jwt token
+    """
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=expires_minutes)
-    to_encode.update({"exp": expire, 'type': token_type})
+    to_encode.update({"exp": expire, 'grant_type': grant_type})
     return jwt.encode(to_encode, secret, algorithm=ALGORITHM)
 
 
-async def get_current_user(
+async def get_user_from_token(
     settings: Settings = Depends(get_settings),
     session: AsyncSession = Depends(get_session),
     token: str = Depends(oauth2),
+    grant_type: Literal['access', 'refresh'] = 'access',
 ) -> UserModel:
     """Get user from jwt.
 
@@ -68,15 +73,35 @@ async def get_current_user(
     )
 
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=ALGORITHM)
         user_id: str = payload.get("sub")
         if user_id is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
 
-    user = await session.get(User, user_id)
+    if payload.get("grant_type") != grant_type:
+        raise credentials_exception
+
+    user = await session.get(User, int(user_id))
     if user is None:
         raise credentials_exception
 
     return UserModel.from_db(user)
+
+
+async def get_current_user(  # noqa: D103
+    settings: Settings = Depends(get_settings),
+    session: AsyncSession = Depends(get_session),
+    token: str = Depends(oauth2),
+) -> UserModel:
+    return await get_user_from_token(settings, session, token, 'access')
+
+
+async def get_current_user_refresh(  # noqa: D103
+    settings: Settings = Depends(get_settings),
+    session: AsyncSession = Depends(get_session),
+    token: str = Depends(oauth2),
+) -> UserModel:
+    return await get_user_from_token(settings, session, token, 'refresh')
