331a3b076d111483e48b801bc7a2ce16836518d6
Mastermind-U <rex49513@gmail.com>
2023-05-30 13:35:06 +0300

    added: cors origins

--- app/web_app.py
+++ app/web_app.py
@@ -1,6 +1,7 @@
 """Multidirectory api module."""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import auth_router, entry_router
 from config import Settings, get_settings
@@ -11,10 +12,18 @@ def create_app(settings: Settings | None = None) -> FastAPI:
     """Create FastAPI app with dependencies overrides."""
     settings = settings or Settings()
     app = FastAPI(name="multidirectory", debug=settings.DEBUG)
+    origins = ["*"]
     app.dependency_overrides = {
         get_settings: lambda: settings,
         get_session: get_async_session,
     }
     app.include_router(auth_router)
     app.include_router(entry_router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     return app
