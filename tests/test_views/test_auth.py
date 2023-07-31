339ef87fe561df1be771338a056ce3d4874633dd
Mastermind-U <rex49513@gmail.com>
2023-07-31 23:45:54 +0300

    fix: isort

--- tests/test_views/test_auth.py
+++ tests/test_views/test_auth.py
@@ -1,6 +1,7 @@
+from unittest.mock import AsyncMock
+
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
-from unittest.mock import AsyncMock
 
 from app.ldap.dialogue import Session
 from app.ldap.ldap_requests import (
