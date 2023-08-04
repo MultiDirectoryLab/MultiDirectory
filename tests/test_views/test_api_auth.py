05e874f6fee7b013260f99fab70e0cd30a788e90
Mastermind-U <rex49513@gmail.com>
2023-08-04 17:09:02 +0300

    added: base user login test

--- tests/test_views/test_api_auth.py
+++ tests/test_views/test_api_auth.py
@@ -23,6 +23,18 @@ async def test_first_setup_and_oauth(http_client, session):
         "matchedDN": "",
         "errorMessage": "",
     }
-    response = await http_client.post("auth/token/get", data={
+    auth = await http_client.post("auth/token/get", data={
         "username": "test", "password": "password"})
+    assert auth.status_code == 200
+
+    login_header = {'Authorization': f"Bearer {auth.json()['access_token']}"}
+
+    response = await http_client.get("auth/me", headers=login_header)
     assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "sam_accout_name": "test",
+        "user_principal_name": "test",
+        "mail": "test@example.com",
+        "display_name": "test",
+    }
