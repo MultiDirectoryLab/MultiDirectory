6d466099bc52c7a0efc5e265fa43fdbc39be4bb8
Mastermind-U <rex49513@gmail.com>
2023-09-06 20:54:19 +0300

    test: groups delete

--- tests/test_views/test_modify.py
+++ tests/test_views/test_modify.py
@@ -6,10 +6,10 @@ from collections import defaultdict
 
 import pytest
 from sqlalchemy import select
-from sqlalchemy.orm import joinedload, subqueryload
+from sqlalchemy.orm import joinedload, selectinload, subqueryload
 
 from app.extra import TEST_DATA, setup_enviroment
-from app.models.ldap3 import Directory, Path
+from app.models.ldap3 import Directory, Group, Path, User
 
 
 @pytest.mark.asyncio()
@@ -88,3 +88,50 @@ async def test_ldap_base_modify(session, settings):
     assert directory.user.mail == "modme@student.of.life.edu"
 
     assert 'posixEmail' not in attributes
+
+
+@pytest.mark.asyncio()
+async def test_ldap_membersip_user_modify_delete(session, settings):
+    """Test ldapadd on server."""
+    await setup_enviroment(session, dn="multidurectory.test", data=TEST_DATA)
+    await session.commit()
+
+    user = TEST_DATA[1]['children'][0]['organizationalPerson']
+
+    dn = "cn=user0,ou=users,dc=multidurectory,dc=test"
+
+    membership = selectinload(Directory.user).selectinload(
+        User.groups).selectinload(
+            Group.directory).selectinload(Directory.path)
+
+    query = select(Directory)\
+        .options(
+            subqueryload(Directory.attributes),
+            joinedload(Directory.user), membership)\
+        .join(Directory.path).filter(Path.path == ["ou=users", "cn=user0"])
+
+    directory = await session.scalar(query)
+
+    assert directory.user.groups
+
+    with tempfile.NamedTemporaryFile("w") as file:
+        file.write((
+            f"dn: {dn}\n"
+            "changetype: modify\n"
+            "delete: memberOf\n"
+            "-\n"
+        ))
+        file.seek(0)
+        proc = await asyncio.create_subprocess_exec(
+            'ldapmodify',
+            '-vvv', '-h', f'{settings.HOST}', '-p', f'{settings.PORT}',
+            '-D', user['sam_accout_name'], '-x', '-w', user['password'],
+            '-f', file.name,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+
+        result = await proc.wait()
+
+    assert result == 0
+    await session.refresh(directory)
+    assert not directory.user.groups
