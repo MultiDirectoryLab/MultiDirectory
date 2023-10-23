477abf59e6c187c1a4dc2a0598bdfca96d27cf09
Mastermind-U <rex49513@gmail.com>
2023-10-23 16:10:59 +0300

    test: test_connect_mfa

--- tests/test_views/test_mfa.py
+++ tests/test_views/test_mfa.py
@@ -1,10 +1,17 @@
 """MFA methods."""
 
+import asyncio
+
 import httpx
 import pytest
+from fastapi import FastAPI
 from sqlalchemy import select
 
+from app.api.auth.oauth2 import authenticate_user, create_token
+from app.api.auth.router_mfa import get_queue_pool, two_factor_protocol
+from app.config import Settings
 from app.extra import TEST_DATA, setup_enviroment
+from app.ldap_protocol.multifactor import MultifactorAPI, get_auth
 from app.models import CatalogueSetting
 
 
@@ -20,7 +27,7 @@ async def test_set_mfa(http_client: httpx.AsyncClient, session):
 
     response = await http_client.post(
         "/multifactor/setup", headers=login_headers,
-        json={'mfa_key': "123", 'mfa_secret': "123"})
+        json={'mfa_key': "123", 'mfa_secret': "123", 'is_ldap_scope': False})
 
     assert response.json() is True
     assert response.status_code == 201
@@ -29,3 +36,85 @@ async def test_set_mfa(http_client: httpx.AsyncClient, session):
         name="mfa_key", value="123"))
     assert await session.scalar(select(CatalogueSetting).filter_by(
         name="mfa_secret", value="123"))
+
+
+@pytest.mark.asyncio()
+async def test_connect_mfa(app: FastAPI, session, http_client: httpx.AsyncClient, settings: Settings):
+    """Test websocket mfa."""
+    await setup_enviroment(session, dn="md.test", data=TEST_DATA)
+    session.add(CatalogueSetting(name='mfa_secret', value=settings.SECRET_KEY))
+    session.add(CatalogueSetting(name='mfa_key', value='123'))
+    await session.commit()
+
+    redirect_url = "example.com"
+
+    class TestMultifactorAPI(MultifactorAPI):
+        async def get_create_mfa(*args, **kwargs):
+            nonlocal redirect_url
+            return redirect_url
+
+    class StubWebSocket:
+        def __init__(self) -> None:
+            self.q = asyncio.Queue(maxsize=1)
+            self.q1 = asyncio.Queue(maxsize=1)
+            self.q2 = asyncio.Queue(maxsize=1)
+
+        async def accept(self):
+            pass
+
+        async def close(self, code=1000, data=None):
+            await self.q2.put((code, data))
+
+        async def send_json(self, data):
+            await self.q1.put(data)
+
+        async def receive_json(self):
+            return await self.q.get()
+
+        async def catch_close(self):
+            return await self.q2.get()
+
+        async def client_send_json(self, data):
+            await self.q.put(data)
+
+        async def client_receive_json(self):
+            return await self.q1.get()
+
+        def url_for(self, *args, **kwargs):
+            return redirect_url
+
+    ws = StubWebSocket()
+
+    mfa = await TestMultifactorAPI.from_di(
+        await get_auth(session), http_client, settings)
+
+    pool = {}
+
+    app.dependency_overrides[get_queue_pool] = lambda: pool
+
+    user = await authenticate_user(session, 'user0', 'password')
+    token = create_token(
+        user.id, settings.SECRET_KEY,
+        settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        grant_type='multifactor',
+        extra_data={'aud': '123'})
+
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(two_factor_protocol(ws, session, mfa, pool, settings))
+        message1 = tg.create_task(ws.client_receive_json())
+
+        tg.create_task(ws.client_send_json(
+            {'username': 'user0', 'password': 'password'}))
+        message2 = tg.create_task(ws.client_receive_json())
+
+        response = await http_client.post('/multifactor/create', data={
+            'accessToken': token})
+
+        assert response.json() is True
+
+        message3 = tg.create_task(ws.client_receive_json())
+
+    assert message1.result() == {'status': 'connected', 'message': ''}
+    assert message2.result() == {'status': 'pending', 'message': redirect_url}
+    assert message3.result() == {'status': 'success', 'message': token}
+    assert await ws.catch_close() == (1000, None)
