4e23f8b71855808bc2835f68a4bc4f7f7a1eb17f
Mastermind-U <rex49513@gmail.com>
2024-02-22 15:21:21 +0300

    test: whoami

---
+++ tests/test_ldap/test_util/test_whoami.py
@@ -0,0 +1,31 @@
+"""Test whoami with ldaputil."""
+
+import asyncio
+
+import pytest
+from app.config import Settings
+from tests.conftest import TestCreds
+
+
+@pytest.mark.asyncio()
+@pytest.mark.usefixtures('setup_session')
+async def test_anonymous_whoami(settings: Settings) -> None:
+    """Test anonymous whoami."""
+    proc = await asyncio.create_subprocess_exec(
+        'ldapwhoami', '-x', '-h', f'{settings.HOST}', '-p', f'{settings.PORT}')
+
+    assert await proc.wait() == 0
+
+
+@pytest.mark.asyncio()
+@pytest.mark.usefixtures('setup_session')
+async def test_binded_whoami(settings: Settings, creds: TestCreds) -> None:
+    """Test anonymous whoami."""
+    proc = await asyncio.create_subprocess_exec(
+        'ldapwhoami', '-x',
+        '-h', f'{settings.HOST}', '-p', f'{settings.PORT}',
+        '-D', creds.un,
+        '-w', creds.pw,
+    )
+
+    assert await proc.wait() == 0
