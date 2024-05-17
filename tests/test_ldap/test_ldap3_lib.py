8c9f31632e8bc70fc11836b29bf27ef03481e30e
Nikita Ulyanov <var.ulyanov@gmail.com>
2024-05-17 17:48:56 +0300

    test: add search by memberOf

--- tests/test_ldap/test_ldap3_lib.py
+++ tests/test_ldap/test_ldap3_lib.py
@@ -32,3 +32,27 @@ async def test_ldap3_search(
 
     assert result
     assert ldap_client.entries
+
+
+@pytest.mark.asyncio()
+@pytest.mark.usefixtures('setup_session')
+@pytest.mark.usefixtures('session')
+async def test_ldap3_search_memberof(
+        ldap_client: Connection,
+        event_loop: BaseEventLoop,
+        creds: TestCreds) -> None:
+    """Test ldap3 search memberof."""
+    member = 'cn=user1,ou=moscow,ou=russia,ou=users,dc=md,dc=test'
+    await event_loop.run_in_executor(
+        None, partial(ldap_client.rebind, user=creds.un, password=creds.pw))
+
+    result = await event_loop.run_in_executor(
+        None,
+        partial(
+            ldap_client.search, 'dc=md,dc=test',
+            '(memberOf=cn=developers,cn=groups,dc=md,dc=test)',
+        ))
+
+    assert result
+    assert ldap_client.entries
+    assert ldap_client.entries[0].entry_dn == member
