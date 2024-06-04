802d28bb1d8c26d24c71a27fe4e49627a0522dad
Mastermind-U <rex49513@gmail.com>
2024-06-04 18:04:28 +0300

    refactor: remove transaction

--- app/ldap_protocol/ldap_requests/add.py
+++ app/ldap_protocol/ldap_requests/add.py
@@ -217,27 +217,27 @@ class AddRequest(BaseRequest):
                 value=str(create_integer_hash(new_dir.name[::-1])),
                 directory=new_dir))
 
-        async with session.begin_nested():
-            try:
-                new_dir.depth = len(path.path)
-                items_to_add.extend([new_dir, path] + attributes)
+        try:
+            new_dir.depth = len(path.path)
+            items_to_add.extend([new_dir, path] + attributes)
 
-                session.add_all(items_to_add)
+            session.add_all(items_to_add)
 
-                if has_no_parent:
-                    new_dir.paths.append(path)
-                else:
-                    path.directories.extend(
-                        [p.endpoint for p in parent.paths + [path]])
-                await session.flush()
-                new_dir.object_sid = await create_object_sid(
-                    session, new_dir.id)
-                await session.commit()
-            except IntegrityError:
-                await session.rollback()
-                yield AddResponse(result_code=LDAPCodes.ENTRY_ALREADY_EXISTS)
+            if has_no_parent:
+                new_dir.paths.append(path)
             else:
-                yield AddResponse(result_code=LDAPCodes.SUCCESS)
+                path.directories.extend(
+                    [p.endpoint for p in parent.paths + [path]])
+            await session.flush()
+
+            new_dir.object_sid = await create_object_sid(
+                    session, new_dir.id)
+            await session.flush()
+        except IntegrityError:
+            await session.rollback()
+            yield AddResponse(result_code=LDAPCodes.ENTRY_ALREADY_EXISTS)
+        else:
+            yield AddResponse(result_code=LDAPCodes.SUCCESS)
 
     @classmethod
     def from_dict(
@@ -253,8 +253,8 @@ class AddRequest(BaseRequest):
         """
         return AddRequest(
             entry=entry,
+            password=password,
             attributes=[
                 PartialAttribute(type=name, vals=vals)
                 for name, vals in attributes.items()],
-            password=password,
         )
