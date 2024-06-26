bd37731f2b2a03505007f1fa617abe8ed37823b7
Mastermind-U <rex49513@gmail.com>
2024-06-26 13:40:29 +0300

    refactor: add krb integration

---
+++ app/ldap_protocol/kerberos.py
@@ -0,0 +1,99 @@
+"""Kerberos config server for MultiDirectory integration.
+
+Copyright (c) 2024 MultiFactor
+License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
+"""
+
+from contextlib import asynccontextmanager
+from typing import Annotated, AsyncIterator
+
+import httpx
+from fastapi import Depends
+
+from config import Settings, get_settings
+
+
+class KerberosMDAPIClient:
+    """KRB server integration."""
+
+    client: httpx.AsyncClient
+
+    class KRBAPIError(Exception):
+        """API Error."""
+
+    def __init__(self, client: httpx.AsyncClient) -> None:
+        """Set client.
+
+        :param httpx.AsyncClient client: httpx
+        """
+        self.client = client
+
+    async def setup(
+        self,
+        domain: str,
+        admin_dn: str,
+        services_dn: str,
+        krbadmin_dn: str,
+        krbadmin_password: str,
+        admin_password: str,
+        stash_password: str,
+        krb5_config: str,
+    ) -> None:
+        """Request Setup."""
+        response = await self.client.post('setup', json={
+            "domain": domain,
+            "admin_dn": admin_dn,
+            "services_dn": services_dn,
+            "krbadmin_dn": krbadmin_dn,
+            "krbadmin_password": krbadmin_password,
+            "admin_password": admin_password,
+            "stash_password": stash_password,
+            'krb5_config': krb5_config.encode().hex(),
+        })
+        if response != 201:
+            raise self.KRBAPIError(response.text)
+
+    async def add_principal(self, name: str, password: str) -> None:
+        """Add request."""
+        response = await self.client.post('principal', json={
+            'name': name, 'password': password})
+
+        if response != 201:
+            raise self.KRBAPIError(response.json())
+
+    async def get_principal(self, name: str) -> dict:
+        """Get request."""
+        response = await self.client.post('principal', data={'name': name})
+        if response != 200:
+            raise self.KRBAPIError(response.json())
+        return response.json()
+
+    async def change_principal_password(
+            self, name: str, password: str) -> None:
+        """Change password request."""
+        response = await self.client.patch('principal', json={
+            'name': name, 'password': password})
+        if response != 200:
+            raise self.KRBAPIError(response.json())
+
+    async def rename_princ(self, name: str, new_name: str) -> None:
+        """Rename request."""
+        response = await self.client.patch('principal', json={
+            'name': name, 'new_name': new_name})
+        if response != 200:
+            raise self.KRBAPIError(response.json())
+
+
+async def get_krb_http_client(
+    settings: Annotated[Settings, Depends(get_settings)],
+) -> AsyncIterator[KerberosMDAPIClient]:
+    """Get async client for DI."""
+    async with httpx.AsyncClient(
+        timeout=30,
+        verify="/certs/krbcert.pem",
+        base_url=settings.KRB5_CONFIG_SERVER,
+    ) as client:
+        yield KerberosMDAPIClient(client)
+
+
+get_krb_ldap_client = asynccontextmanager(get_krb_http_client)
