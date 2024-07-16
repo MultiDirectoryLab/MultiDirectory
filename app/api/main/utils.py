ffd81bfd90d0f00054c7544de4a2680559b7b634
Mastermind-U <rex49513@gmail.com>
2024-07-16 14:17:46 +0300

    refactor: split deps

--- app/api/main/utils.py
+++ app/api/main/utils.py
@@ -8,16 +8,26 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api.auth import User, get_current_user
 from config import Settings, get_settings
 from ldap_protocol.dialogue import Session as LDAPSession
-from ldap_protocol.kerberos import get_kerberos_class
+from ldap_protocol.kerberos import AbstractKadmin, get_kerberos_class
 from models.database import get_session
+
+
+async def get_krb_class(
+    session: Annotated[AsyncSession, Depends(get_session)],
+) -> AbstractKadmin:
+    """Get kerberos class.
+
+    :param Annotated[AsyncSession, Depends session: db
+    :return AbstractKadmin: wrapper
+    """
+    return await get_kerberos_class(session)
 
 
 async def get_ldap_session(
     user: Annotated[User, Depends(get_current_user)],
     settings: Annotated[Settings, Depends(get_settings)],
-    session: Annotated[AsyncSession, Depends(get_session)],
+    cls: Annotated[AbstractKadmin, Depends(get_krb_class)],
 ) -> AsyncIterator[LDAPSession]:
     """Create LDAP session."""
-    cls = await get_kerberos_class(session)
     async with cls.get_krb_ldap_client(settings) as kadmin:
         yield LDAPSession(user=user, settings=settings, kadmin=kadmin)
