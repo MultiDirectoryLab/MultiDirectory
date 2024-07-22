7fe36b537b625ffc927da8fef7be13eee3cdea03
Mastermind-U <rex49513@gmail.com>
2024-07-22 18:54:20 +0300

    add: ioc container setup

--- app/web_app.py
+++ app/web_app.py
@@ -4,6 +4,11 @@ Copyright (c) 2024 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
+from dishka import make_async_container
+from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -15,12 +20,17 @@ from api import (
     network_router,
     pwd_router,
 )
-from config import VENDOR_VERSION, Settings, get_settings
-from models.database import create_get_async_session, get_session
+from config import VENDOR_VERSION, Settings
+from ioc import MainProvider, MFAProvider
 
 
 def create_app(settings: Settings | None = None) -> FastAPI:
     """Create FastAPI app with dependencies overrides."""
+    @asynccontextmanager
+    async def _lifespan(app: FastAPI) -> AsyncIterator[None]:
+        yield
+        await app.state.dishka_container.close()
+
     settings = settings or Settings()
 
     app = FastAPI(
@@ -29,12 +39,9 @@ def create_app(settings: Settings | None = None) -> FastAPI:
         debug=settings.DEBUG,
         root_path="/api",
         version=VENDOR_VERSION,
+        lifespan=_lifespan,
     )
     origins = ["*"]
-    app.dependency_overrides = {
-        get_settings: lambda: settings,
-        get_session: create_get_async_session(settings),
-    }
     app.include_router(auth_router)
     app.include_router(entry_router)
     app.include_router(network_router)
@@ -48,4 +55,9 @@ def create_app(settings: Settings | None = None) -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+    container = make_async_container(
+        MainProvider(), MFAProvider(),
+        context={Settings: settings})
+
+    setup_dishka(container, app)
     return app
