09c0d72fd1241b80a54e635645a6f9b57311721f
Mastermind-U <rex49513@gmail.com>
2024-07-22 20:13:12 +0300

    refactor: remove get_kadmin

--- app/api/main/utils.py
+++ app/api/main/utils.py
@@ -3,31 +3,18 @@
 from typing import Annotated, AsyncIterator
 
 from dishka import FromDishka
+from dishka.integrations.fastapi import inject
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.auth import User, get_current_user
-from config import Settings
-from ldap_protocol.dialogue import LDAPSession as LDAPSession
-from ldap_protocol.kerberos import AbstractKadmin, get_kerberos_class
+from ldap_protocol.dialogue import LDAPSession
 
 
-async def get_kadmin(
-    session: FromDishka[AsyncSession],
-    settings: FromDishka[Settings],
-) -> AsyncIterator[AbstractKadmin]:
-    """Get kerberos class.
-
-    :param Annotated[AsyncSession, Depends session: db
-    :return AbstractKadmin: wrapper
-    """
-    cls = await get_kerberos_class(session)
-    async with cls.get_krb_ldap_client(settings) as kadmin:
-        yield kadmin
-
-
+@inject
 async def get_ldap_session(
+    ldap_session: FromDishka[LDAPSession],
     user: Annotated[User, Depends(get_current_user)],
 ) -> AsyncIterator[LDAPSession]:
     """Create LDAP session."""
-    return LDAPSession(user=user)
+    await ldap_session.set_user(user)
+    return ldap_session
