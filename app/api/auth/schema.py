3b74fb332dce4b56ff694e8fbb51a29d39a808ce
iyashnov <x13777x@gmail.com>
2024-07-29 11:49:43 +0300

    add: dn to user for API response

--- app/api/auth/schema.py
+++ app/api/auth/schema.py
@@ -12,6 +12,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel, EmailStr, SecretStr, computed_field, validator
 
 from models.ldap3 import User as DBUser
+from ldap_protocol.utils import get_path_dn
 
 domain_regex = "^((?!-)[A-Za-z0-9-]" + "{1,63}(?<!-)\\.)" + "+[A-Za-z]{2,6}"
 domain_re = re.compile(domain_regex)
@@ -53,12 +54,18 @@ class User(BaseModel):
     mail: str
     display_name: str
     directory_id: int
+    base_distinguished_name: str
 
     access_type: Literal['access', 'refresh', 'multifactor']
     exp: int
 
     @classmethod
-    def from_db(cls, user: DBUser, access: str, exp: int = 0) -> 'User':
+    def from_db(
+            cls,
+            user: DBUser,
+            access: str,
+            base_distinguished_name: str,
+            exp: int = 0) -> 'User':
         """Create model from db model."""
         return cls(
             id=user.id,
@@ -69,7 +76,12 @@ class User(BaseModel):
             access_type=access,
             exp=exp,
             directory_id=user.directory_id,
+            base_distinguished_name=base_distinguished_name
         )
+
+    @computed_field
+    def distinguished_name(self) -> str:
+        return get_path_dn(self.directory.path, self.base_distinguished_name)
 
 
 class SetupRequest(BaseModel):
