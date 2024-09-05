98a4bb1f86dbd5b533747628264e6e984c53c47d
Mastermind-U <rex49513@gmail.com>
2024-09-05 14:07:01 +0300

    add: default admin pwdLastSet

--- app/api/auth/router.py
+++ app/api/auth/router.py
@@ -25,7 +25,11 @@ from ldap_protocol.password_policy import (
     post_save_password_actions,
 )
 from ldap_protocol.user_account_control import UserAccountControlFlag, get_uac
-from ldap_protocol.utils import get_base_directories, set_last_logon_user
+from ldap_protocol.utils import (
+    ft_now,
+    get_base_directories,
+    set_last_logon_user,
+)
 from models.ldap3 import CatalogueSetting, Directory, Group
 from models.ldap3 import User as DBUser
 from security import get_password_hash
@@ -300,6 +304,7 @@ async def first_setup(
                             "posixAccount",
                             "shadowAccount",
                         ],
+                        "pwdLastSet": [ft_now()],
                         "loginShell": ["/bin/bash"],
                         "uidNumber": ["1000"],
                         "gidNumber": ["10000"],
