26e95d78544b6695d88f3d6cd45f60a2e7e0633f
Mastermind-U <rex49513@gmail.com>
2024-09-09 15:05:40 +0300

    fix: rename domain

--- app/extra/generate_cert.sh
+++ app/extra/generate_cert.sh
@@ -4,5 +4,5 @@ test -f /certs/krbcert.pem && echo "CERT EXISTS, SKIPPING..." || openssl \
     req -nodes -new -x509 \
     -keyout /certs/krbkey.pem \
     -out /certs/krbcert.pem \
-    -addext "subjectAltName=DNS:kadmin" \
-    -subj '/C=RU/ST=Moscow/L=Moscow/O=Global Security/OU=Multifactor/CN=kadmin'
+    -addext "subjectAltName=DNS:kadmin-api" \
+    -subj '/C=RU/ST=Moscow/L=Moscow/O=Global Security/OU=Multifactor/CN=kadmin-api'
