acd07a340da930f46c97b7f9ab16aa75c3038ac6
Nikita Ulyanov <var.ulyanov@gmail.com>
2024-09-17 16:22:15 +0300

    refactor: remove and_

--- app/extra/scripts/uac_sync.py
+++ app/extra/scripts/uac_sync.py
@@ -5,7 +5,7 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 from sqlalchemy import Integer, String, cast, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql import and_, func, select
+from sqlalchemy.sql import func, select
 
 from models.ldap3 import Attribute, User
 
@@ -16,15 +16,14 @@ async def update_uac_accounts(session: AsyncSession) -> None:
     :param AsyncSession session: db
     """
     subquery = select(User.directory_id).where(
-        and_(
-            User.account_exp < func.now(),
-            User.directory_id == Attribute.directory_id)).scalar_subquery()
+        User.account_exp < func.now(),
+        User.directory_id == Attribute.directory_id).scalar_subquery()
     await session.execute(  # noqa
         update(Attribute)
         .values(value=cast(cast(Attribute.value, Integer).op('|')(2), String))
-        .where(and_(
+        .where(
             cast(Attribute.value, Integer).op('&')(2) == 0,
             Attribute.directory_id.in_(subquery),
-            Attribute.name == 'userAccountControl'))
+            Attribute.name == 'userAccountControl')
         .execution_options(synchronize_session=False))
     await session.commit()
