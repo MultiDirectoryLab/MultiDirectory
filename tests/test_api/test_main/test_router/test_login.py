38c4b2d385677f9589f61cdc871e661f67352183
Mastermind-U <rex49513@gmail.com>
2024-09-30 14:44:54 +0300

    test: refresh and logout flow

--- tests/test_api/test_main/test_router/test_login.py
+++ tests/test_api/test_main/test_router/test_login.py
@@ -3,16 +3,18 @@
 Copyright (c) 2024 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
+import httpx
 import pytest
 from fastapi import status
-from httpx import AsyncClient
 
 from app.ldap_protocol.dialogue import Operation
+from tests.conftest import TestCreds
 
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures('session')
-async def test_api_before_setup(unbound_http_client: AsyncClient) -> None:
+async def test_api_before_setup(
+        unbound_http_client: httpx.AsyncClient) -> None:
     """Test api before setup."""
     response = await unbound_http_client.get("auth/me")
 
@@ -21,10 +23,9 @@ async def test_api_before_setup(unbound_http_client: AsyncClient) -> None:
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures('adding_test_user')
-@pytest.mark.usefixtures('setup_session')
 @pytest.mark.usefixtures('session')
 async def test_api_auth_after_change_account_exp(
-        http_client: AsyncClient) -> None:
+        http_client: httpx.AsyncClient) -> None:
     """Test api auth."""
     await http_client.patch(
         "/entry/update",
@@ -64,7 +65,6 @@ async def test_api_auth_after_change_account_exp(
                 },
             ],
         },
-        headers=login_headers,
     )
     auth = await http_client.post(
         "auth/token/get",
@@ -73,4 +73,28 @@ async def test_api_auth_after_change_account_exp(
             "password": 'P@ssw0rd',
         })
 
-    assert auth.json()['access_token']
+    assert auth.cookies.get('access_token')
+
+
+@pytest.mark.usefixtures('setup_session')
+async def test_refresh_and_logout_flow(
+        unbound_http_client: httpx.AsyncClient,
+        creds: TestCreds) -> None:
+    """Test login, refresh and logout cookie flow."""
+    await unbound_http_client.post(
+        "auth/token/get",
+        data={"username": creds.un, "password": creds.pw})
+
+    refresh_token = unbound_http_client.cookies.get('refresh_token')
+    old_token = unbound_http_client.cookies.get('access_token')
+
+    assert old_token
+    assert refresh_token
+
+    response = await unbound_http_client.post("/api/auth/token/refresh")
+    assert response.status_code == 200
+    assert old_token != response.cookies.get('access_token')
+
+    await unbound_http_client.delete("auth/token/refresh")
+
+    assert not unbound_http_client.cookies
