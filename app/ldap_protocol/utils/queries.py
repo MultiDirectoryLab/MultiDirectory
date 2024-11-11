89b978c705c85271389f5c02ad5ead9414956204
iyashnov <x13777x@gmail.com>
2024-11-11 12:12:58 +0300

    refactor: added access policies to load in get_group

--- app/ldap_protocol/utils/queries.py
+++ app/ldap_protocol/utils/queries.py
@@ -11,7 +11,12 @@ from zoneinfo import ZoneInfo
 from asyncstdlib.functools import cache
 from sqlalchemy import Column, func, or_, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import InstrumentedAttribute, defaultload, selectinload
+from sqlalchemy.orm import (
+    InstrumentedAttribute,
+    defaultload,
+    joinedload,
+    selectinload,
+)
 from sqlalchemy.sql.expression import ColumnElement
 
 from models import Attribute, Directory, Group, NetworkPolicy, User
@@ -103,7 +108,9 @@ async def get_group(dn: str | ENTRY_TYPE, session: AsyncSession) -> Directory:
         if dn_is_base_directory(base_directory, dn):
             raise ValueError("Cannot set memberOf with base dn")
 
-    query = select(Directory).options(defaultload(Directory.group))
+    query = select(Directory).options(
+        joinedload(Directory.group).selectinload(Group.access_policies),
+    )
 
     if validate_entry(dn):
         query = query.filter(Directory.path == get_search_path(dn))
