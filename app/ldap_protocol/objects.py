7de6727fdcc6580e482007c9e3a61d8c10a29f84
Nikita Ulyanov <var.ulyanov@gmail.com>
2024-11-25 13:15:10 +0300

    add: audit event type

--- app/ldap_protocol/objects.py
+++ app/ldap_protocol/objects.py
@@ -42,9 +42,52 @@ class DerefAliases(IntEnum):
 
 
 class LDAPMatchingRule(StrEnum):
-    """Enum for LDAP Matching Rules (extensibleMatch)."""
+    """Enum for LDAP Matching Rules (extensibleMatch).
+
+    ```
+    LDAP_MATCHING_RULE_BIT_AND = "1.2.840.113556.1.4.803"
+    LDAP_MATCHING_RULE_BIT_OR = "1.2.840.113556.1.4.804"
+    LDAP_MATCHING_RULE_TRANSITIVE_EVAL = "1.2.840.113556.1.4.1941"
+    LDAP_MATCHING_RULE_DN_WITH_DATA = "1.2.840.113556.1.4.2253"
+    ```
+    """
 
     LDAP_MATCHING_RULE_BIT_AND = "1.2.840.113556.1.4.803"
     LDAP_MATCHING_RULE_BIT_OR = "1.2.840.113556.1.4.804"
     LDAP_MATCHING_RULE_TRANSITIVE_EVAL = "1.2.840.113556.1.4.1941"
     LDAP_MATCHING_RULE_DN_WITH_DATA = "1.2.840.113556.1.4.2253"
+
+
+class AuditEventType(StrEnum):
+    """Enum for audit event type.
+
+    ```
+    LDAP_ADD = "ldap_add"
+    LDAP_AUTH = "ldap_auth"
+    LDAP_DELETE = "ldap_delete"
+    LDAP_MODIFY = "ldap_modify"
+    LDAP_MODIFY_DN = "ldap_modify_dn"
+    LDAP_EXTEND = "ldap_extend"
+
+    API_AUTH = "api_auth"
+    API_ADD = "api_add"
+    API_DELETE = "api_delete"
+    API_MODIFY = "api_modify"
+    API_MODIFY_DN = "api_modify_dn"
+    API_EXTEND = "api_extend"
+    ```
+    """
+
+    LDAP_ADD = "ldap_add"
+    LDAP_AUTH = "ldap_auth"
+    LDAP_DELETE = "ldap_delete"
+    LDAP_MODIFY = "ldap_modify"
+    LDAP_MODIFY_DN = "ldap_modify_dn"
+    LDAP_EXTEND = "ldap_extend"
+
+    API_AUTH = "api_auth"
+    API_ADD = "api_add"
+    API_DELETE = "api_delete"
+    API_MODIFY = "api_modify"
+    API_MODIFY_DN = "api_modify_dn"
+    API_EXTEND = "api_extend"
