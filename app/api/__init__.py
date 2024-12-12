775ffd3d96050a22870c5a188a4546c844e958ec
Nikita Ulyanov <var.ulyanov@gmail.com>
2024-12-12 13:19:54 +0300

    add: introduce shadow router for MFA proxy requests

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -12,14 +12,16 @@ from .main.dns_router import dns_router
 from .main.krb5_router import krb5_router
 from .main.router import entry_router
 from .network.router import network_router
+from .shadow.router import shadow_router
 
 __all__ = [
     "auth_router",
-    "entry_router",
-    "network_router",
     "mfa_router",
     "pwd_router",
-    "krb5_router",
     "access_policy_router",
     "dns_router",
+    "krb5_router",
+    "entry_router",
+    "network_router",
+    "shadow_router",
 ]
