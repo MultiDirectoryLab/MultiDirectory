40eae75a70e0b0dfb9c79ab2610f4016fb79d5ad
Nikita Ulyanov <var.ulyanov@gmail.com>
2024-12-12 14:54:57 +0300

    fix: enhance proxy request logic to validate principal against directory entries

--- app/api/shadow/router.py
+++ app/api/shadow/router.py
@@ -6,19 +6,37 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 from typing import Annotated
 
 from dishka import FromDishka
+from dishka.integrations.fastapi import inject
 from fastapi import APIRouter, Body
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ldap_protocol.multifactor import MultifactorAPI
+from models import Directory
 
 shadow_router = APIRouter()
 
 
 @shadow_router.post("/get/push/principal")
+@inject
 async def proxy_request(
     principal: Annotated[str, Body(embed=True)],
     mfa: FromDishka[MultifactorAPI],
     session: FromDishka[AsyncSession],
 ) -> bool:
     """Proxy request to mfa."""
-    return await mfa.ldap_validate_mfa(principal, None) if mfa else False
+    if not mfa:
+        return False
+
+    for _ in range(3):
+        if await session.scalar(
+            select(Directory)
+            .filter(Directory.name == principal),
+        ):
+            break
+
+        principal = principal[:-1]
+    else:
+        return False
+
+    return await mfa.ldap_validate_mfa(principal, None)
