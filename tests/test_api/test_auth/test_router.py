c404c812ef51b7c450e4bde3d40b720f3e05ced3
Naksen <khannanov.rus@gmail.com>
2024-12-22 22:31:05 +0300

    test: add universal test to verify various auth scenarios

--- tests/test_api/test_auth/test_router.py
+++ tests/test_api/test_auth/test_router.py
@@ -10,12 +10,14 @@ from fastapi import status
 from httpx import AsyncClient
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, selectinload
 
+from config import Settings
 from ldap_protocol.dialogue import LDAPCodes, Operation
 from ldap_protocol.kerberos import AbstractKadmin
+from ldap_protocol.multifactor import MFAStatus, update_mfa_status
 from ldap_protocol.utils.queries import get_search_path
-from models import Directory, Group
+from models import CatalogueSetting, Directory, Group, MFAFlags, NetworkPolicy
 
 
 async def apply_user_account_control(
@@ -346,3 +348,185 @@ async def test_lock_and_unlock_user(
 
     assert "nsAccountLock" not in attrs
     assert "shadowExpire" not in attrs
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    (
+        "is_group_in_policy",
+        "is_user_in_group",
+        "is_group_in_mfa_groups",
+        "is_http_enabled",
+        "mfa_state",
+        "mfa_status",
+        "bypass_no_connection",
+        "bypass_service_failure",
+        "expected_status",
+    ),
+    [
+        # 1. User is not in the group => 403
+        (
+            True, False, False, True, None, MFAFlags.DISABLED, False, False,
+            status.HTTP_403_FORBIDDEN,
+        ),
+
+        # 2. User is in the group => 200
+        # (if there are no other blocking reasons)
+        (
+            True, True, False, True, None, MFAFlags.DISABLED, False, False,
+            status.HTTP_200_OK,
+        ),
+
+        # 3. Http protocol is disabled => 403
+        (
+            True, True, False, False, None, MFAFlags.DISABLED, False, False,
+            status.HTTP_403_FORBIDDEN,
+        ),
+
+        # 4. MFA MISCONFIGURED + policy.mfa_status=ENABLED =>
+        # waiting for the bypass => 200
+        (
+            True, True, False, True, MFAStatus.MISCONFIGURED, MFAFlags.ENABLED,
+            False, False,
+            status.HTTP_200_OK,
+        ),
+
+        # 5. MFA UNAVAILABLE + policy.mfa_status=ENABLED +
+        # bypass_no_connection=True => 200
+        (
+            True, True, False, True, MFAStatus.UNAVAILABLE, MFAFlags.ENABLED,
+            True, False,
+            status.HTTP_200_OK,
+        ),
+
+        # 6. MFA UNAVAILABLE + policy.mfa_status=ENABLED +
+        # bypass_no_connection=False => 403
+        (
+            True, True, False, True, MFAStatus.UNAVAILABLE, MFAFlags.ENABLED,
+            False, False,
+            status.HTTP_403_FORBIDDEN,
+        ),
+
+        # 7. MFA FAULTED + policy.mfa_status=ENABLED +
+        # bypass_service_failure=True => 200
+        (
+            True, True, False, True, MFAStatus.FAULTED, MFAFlags.ENABLED,
+            False, True,
+            status.HTTP_200_OK,
+        ),
+
+        # 8. MFA FAULTED + policy.mfa_status=ENABLED +
+        # bypass_service_failure=False => 403
+        (
+            True, True, False, True, MFAStatus.FAULTED, MFAFlags.ENABLED,
+            False, False,
+            status.HTTP_403_FORBIDDEN,
+        ),
+
+        # 9. MFA AVAILABLE + policy.mfa_status=WHITELIST +
+        # mfa_groups are empty => 426
+        (
+            True, True, False, True, MFAStatus.AVAILABLE, MFAFlags.WHITELIST,
+            True, True,
+            status.HTTP_426_UPGRADE_REQUIRED,
+        ),
+
+        # 10. MFA AVAILABLE + policy.mfa_status=WHITELIST +
+        # user group is in mfa_groups => 426
+        (
+            True, True, True, True, MFAStatus.AVAILABLE, MFAFlags.WHITELIST,
+            True, True,
+            status.HTTP_426_UPGRADE_REQUIRED,
+        ),
+
+        # 11. MFA AVAILABLE + policy.mfa_status=WHITELIST +
+        # user group is not in mfa_groups => 403
+        (
+            False, False, True, True, MFAStatus.AVAILABLE, MFAFlags.WHITELIST,
+            True, True,
+            status.HTTP_403_FORBIDDEN,
+        ),
+    ],
+)
+async def test_auth_universal(
+    http_client: AsyncClient,
+    session: AsyncSession,
+    settings: Settings,
+    # test params:
+    is_group_in_policy: bool,
+    is_user_in_group: bool,
+    is_group_in_mfa_groups: bool,
+    is_http_enabled: bool,
+    mfa_state: MFAStatus | None,
+    mfa_status: MFAFlags,
+    bypass_no_connection: bool,
+    bypass_service_failure: bool,
+    expected_status: int,
+):
+    """A universal test that checks different authorization scenarios."""
+    policy = await session.scalar(
+        select(NetworkPolicy)
+        .options(
+            selectinload(NetworkPolicy.groups),
+            selectinload(NetworkPolicy.mfa_groups),
+        )
+        .limit(1),
+    )
+
+    # Clearing all settings just in case
+    policy.groups.clear()
+    policy.mfa_groups.clear()
+    policy.is_http = True
+    policy.mfa_status = MFAFlags.DISABLED
+
+    policy.bypass_no_connection = False
+    policy.bypass_service_failure = False
+
+    await session.commit()
+
+    # Process "is_user_in_group"
+    directory_name = "domain admins" if is_user_in_group else "developers"
+    directory = await session.scalar(select(Directory).filter(
+        Directory.name == directory_name))
+    group = await session.scalar(select(Group).filter(
+        Group.directory == directory))
+
+    # Process "is_group_in_policy"
+    if is_group_in_policy:
+        policy.groups.append(group)
+        await session.commit()
+
+    # Process "is_group_in_mfa_groups"
+    if is_group_in_mfa_groups:
+        policy.mfa_groups.append(group)
+        await session.commit()
+
+    # Process "is_http_enabled"
+    policy.is_http = is_http_enabled
+    await session.commit()
+
+    # Process "mfa_state"
+    session.add(
+        CatalogueSetting(name='mfa_secret', value=settings.SECRET_KEY),
+    )
+    session.add(CatalogueSetting(name='mfa_key', value='123'))
+    if mfa_state is not None:
+        await update_mfa_status(session, mfa_state)
+
+    # Process "mfa_status"
+    policy.mfa_status = mfa_status
+
+    # Process "bypass_no_connection" and "bypass_service_failure"
+    policy.bypass_no_connection = bypass_no_connection
+    policy.bypass_service_failure = bypass_service_failure
+    await session.commit()
+
+    response = await http_client.post(
+        "auth/token/get",
+        data={
+            "username": "user0",
+            "password": "password",
+        },
+    )
+
+    assert response.status_code == expected_status
