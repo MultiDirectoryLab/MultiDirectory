b004541bd0766079f907060c16d2007fdc468bc7
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-01-10 00:22:31 +0300

    refactor: handle NoFactoryError in dependency resolution

--- app/ldap_protocol/dependency.py
+++ app/ldap_protocol/dependency.py
@@ -7,6 +7,7 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 from typing import Any, Callable, TypeVar, get_type_hints
 
 from dishka import AsyncContainer
+from dishka.exceptions import NoFactoryError
 
 T = TypeVar("T", bound=Callable)
 
@@ -18,11 +19,12 @@ async def resolve_deps(func: T, container: AsyncContainer) -> dict[str, Any]:
     :param AsyncContainer container: IoC container
     :return dict[str, Any]: kwargs for func
     """
-    hints = get_type_hints(func)
-    del hints["return"]
     kwargs = {}
 
-    for arg_name, hint in hints.items():
-        kwargs[arg_name] = await container.get(hint)
+    for arg_name, hint in get_type_hints(func).items():
+        try:
+            kwargs[arg_name] = await container.get(hint)
+        except NoFactoryError:
+            pass
 
     return kwargs
