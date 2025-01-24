a424ef7e119cf21a1d5cb20a432987f88e86df14
Mastermind-U <rex49513@gmail.com>
2025-01-24 18:30:12 +0300

    test: add session workflow tests for session creation and LDAP binding

---
+++ tests/test_api/test_auth/test_sessions.py
@@ -0,0 +1,92 @@
+"""Test session workflow."""
+
+import asyncio
+
+import pytest
+from dishka import AsyncContainer
+from httpx import AsyncClient
+from ldap3 import Connection
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ldap_protocol.session_storage import SessionStorage
+from ldap_protocol.utils.queries import get_user
+from tests.conftest import TestCreds
+
+
+@pytest.fixture
+async def storage(container: AsyncContainer) -> SessionStorage:
+    """Return session storage."""
+    async with container() as c:
+        return await c.get(SessionStorage)
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("setup_session")
+async def test_session_creation(
+    unbound_http_client: AsyncClient,
+    creds: TestCreds,
+    storage: SessionStorage,
+    session: AsyncSession,
+) -> None:
+    """Test session creation."""
+    user = await get_user(session, creds.un)
+    assert user
+    assert not await storage.get_user_sessions(user.id)
+
+    response = await unbound_http_client.post(
+        "auth/token/get",
+        data={"username": creds.un, "password": creds.pw},
+    )
+
+    assert response.cookies.get("id")
+    assert response.status_code == 200
+
+    sessions = await storage.get_user_sessions(user.id)
+
+    assert sessions
+
+    key = list(sessions.keys())[0]
+
+    assert sessions[key]["id"] == user.id
+    assert sessions[key]["issued"]
+    assert sessions[key]["ip"]
+    assert sessions[key]["sign"]
+
+    await storage.clear_user_sessions(user.id)
+    assert not await storage.get_user_sessions(user.id)
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("setup_session")
+async def test_session_creation_ldap_bind_unbind(
+    creds: TestCreds,
+    storage: SessionStorage,
+    session: AsyncSession,
+    ldap_client: Connection,
+    event_loop: asyncio.AbstractEventLoop,
+) -> None:
+    """Test session creation for ldap protocol."""
+    user = await get_user(session, creds.un)
+    assert user
+    assert not await storage.get_user_sessions(user.id)
+
+    result = await event_loop.run_in_executor(
+        None, ldap_client.rebind, creds.un, creds.pw)
+
+    assert result
+    assert ldap_client.bound
+
+    sessions = await storage.get_user_sessions(user.id)
+
+    assert sessions
+
+    key = list(sessions.keys())[0]
+
+    assert sessions[key]["id"] == user.id
+    assert sessions[key]["issued"]
+    assert sessions[key]["ip"]
+
+    result = await event_loop.run_in_executor(None, ldap_client.unbind)
+    assert not ldap_client.bound
+
+    assert not await storage.get_user_sessions(user.id)
