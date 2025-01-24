a90443acb229aa91c8859dc33a7e7e9a87eb3421
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-01-24 13:56:34 +0300

    refactor: enhance ProtocolOp enum with unique decorator and update operation values

--- app/ldap_protocol/objects.py
+++ app/ldap_protocol/objects.py
@@ -4,7 +4,7 @@ Copyright (c) 2024 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
-from enum import IntEnum, StrEnum
+from enum import IntEnum, StrEnum, unique
 
 
 class Scope(IntEnum):
@@ -110,30 +110,55 @@ class AuditOperation(StrEnum):
     BITWISE_AND = "&"
 
 
+@unique
 class ProtocolOp(IntEnum):
     """
     Enum for LDAP protocol operations.
 
     Attributes:
         BIND_REQUEST: Represents the BindRequest operation (0)
-        UNBIND_REQUEST: Represents the UnbindRequest operation (1)
+        BIND_RESPONSE: Represents the BindResponse operation (1)
+        UNBIND_REQUEST: Represents the UnbindRequest operation (2)
         SEARCH_REQUEST: Represents the SearchRequest operation (3)
+        SEARCH_RESULT_ENTRY: Represents the SearchResultEntry operation (4)
+        SEARCH_RESULT_DONE: Represents the SearchResultDone operation (5)
         MODIFY_REQUEST: Represents the ModifyRequest operation (6)
+        MODIFY_RESPONSE: Represents the ModifyResponse operation (7)
         ADD_REQUEST: Represents the AddRequest operation (8)
+        ADD_RESPONSE: Represents the AddResponse operation (9)
         DELETE_REQUEST: Represents the DelRequest operation (10)
+        DELETE_RESPONSE: Represents the DelResponse operation (11)
         MODIFY_DN_REQUEST: Represents the ModifyDNRequest operation (12)
+        MODIFY_DN_RESPONSE: Represents the ModifyDNResponse operation (13)
         COMPARE_REQUEST: Represents the CompareRequest operation (14)
+        COMPARE_RESPONSE: Represents the CompareResponse operation (15)
         ABANDON_REQUEST: Represents the AbandonRequest operation (16)
+        SEARCH_RESULT_REFERENCE: Represents the SearchResultReference operation
+                                 (19)
         EXTENDED_REQUEST: Represents the ExtendedRequest operation (23)
+        EXTENDED_RESPONSE: Represents the ExtendedResponse operation (24)
+        INTERMEDIATE_RESPONSE: Represents the IntermediateResponse operation
+                               (25)
     """
 
     BIND_REQUEST = 0
-    UNBIND_REQUEST = 1
+    BIND_RESPONSE = 1
+    UNBIND_REQUEST = 2
     SEARCH_REQUEST = 3
+    SEARCH_RESULT_ENTRY = 4
+    SEARCH_RESULT_DONE = 5
     MODIFY_REQUEST = 6
+    MODIFY_RESPONSE = 7
     ADD_REQUEST = 8
+    ADD_RESPONSE = 9
     DELETE_REQUEST = 10
+    DELETE_RESPONSE = 11
     MODIFY_DN_REQUEST = 12
+    MODIFY_DN_RESPONSE = 13
     COMPARE_REQUEST = 14
+    COMPARE_RESPONSE = 15
     ABANDON_REQUEST = 16
+    SEARCH_RESULT_REFERENCE = 19
     EXTENDED_REQUEST = 23
+    EXTENDED_RESPONSE = 24
+    INTERMEDIATE_RESPONSE = 25
