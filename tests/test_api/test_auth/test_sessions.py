fcc12b9e9847ff4ecd42efdc60083e407fc6e759
Mastermind-U <rex49513@gmail.com>
2025-01-27 11:58:04 +0300

    test: add session API tests for user retrieval and deletion

--- tests/test_api/test_auth/test_sessions.py
+++ tests/test_api/test_auth/test_sessions.py
@@ -90,3 +90,35 @@ async def test_session_creation_ldap_bind_unbind(
     assert not ldap_client.bound
 
     assert not await storage.get_user_sessions(user.id)
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("setup_session")
+async def test_session_api(
+    creds: TestCreds,
+    http_client: AsyncClient,
+    storage: SessionStorage,
+    session: AsyncSession,
+) -> None:
+    """Test session api."""
+    response = await http_client.get(f"session/{creds.un}")
+
+    assert response.status_code == 200
+    user = await get_user(session, creds.un)
+    assert user
+    rdata = response.json()
+    storage_data = await storage.get_user_sessions(user.id)
+
+    for k, data in rdata.items():
+        assert storage_data[k]["id"] == data["id"]
+        assert storage_data[k]["ip"] == data["ip"]
+        # pydantic timezone representation is different from ISO
+        assert storage_data[k]["issued"][:-6] == data["issued"][:-1]
+        assert storage_data[k]["sign"] == data["sign"]
+
+    response = await http_client.get(f"session/{creds.un}123")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "User not found."
+
+    response = await http_client.delete(f"session/{creds.un}")
+    assert response.status_code == 204
