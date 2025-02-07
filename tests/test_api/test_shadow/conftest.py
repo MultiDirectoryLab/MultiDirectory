5915416d2c0db3f4d099e415c796c2684f7f65e5
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-02-07 13:38:57 +0300

    tests: add shadow_api router

---
+++ tests/test_api/test_shadow/conftest.py
@@ -0,0 +1,148 @@
+"""Create network policies and users for shadow api tests.
+
+Copyright (c) 2025 MultiFactor
+License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
+"""
+
+import pytest_asyncio
+from fastapi import status
+from httpx import AsyncClient
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ldap_protocol.ldap_codes import LDAPCodes
+from models import CatalogueSetting
+
+
+class ProxyRequestModel(BaseModel):
+    """Model for the proxy request.
+
+    Attributes:
+        principal: Unique user identifier
+        ip: IP address from which the request is made
+    """
+
+    principal: str
+    ip: str
+
+
+@pytest_asyncio.fixture
+async def adding_mfa_keys(session: AsyncSession) -> None:
+    """Test add user like keycloak."""
+    session.add(
+        CatalogueSetting(name='mfa_secret', value='123'),
+    )
+    session.add(CatalogueSetting(name='mfa_key', value='123'))
+    session.add(
+        CatalogueSetting(name='mfa_key_ldap', value='123'),
+    )
+    session.add(CatalogueSetting(name='mfa_secret_ldap', value='123'))
+    await session.commit()
+
+
+@pytest_asyncio.fixture
+async def adding_mfa_user_and_group(
+    http_client: AsyncClient,
+    unbound_http_client: AsyncClient,
+) -> dict:
+    """Add mfa user and group."""
+    response = await http_client.post(
+        "/entry/add",
+        json={
+            "entry": "cn=mfa_group,cn=groups,dc=md,dc=test",
+            "password": None,
+            "attributes": [
+                {
+                    "type": "name",
+                    "vals": ["mfa_group"],
+                },
+                {
+                    "type": "cn",
+                    "vals": ["mfa_group"],
+                },
+                {
+                    "type": "objectClass",
+                    "vals": ["organization", "top", "group"],
+                },
+            ],
+        },
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert data["resultCode"] == LDAPCodes.SUCCESS
+
+    test_user_dn = "cn=mfa_user,dc=md,dc=test"
+    test_user_email = "mfa_user@md.test"
+    user_password = "P@ssw0rd"  # noqa: S105
+
+    response = await http_client.post(
+        "/entry/add",
+        json={
+            "entry": test_user_dn,
+            "password": user_password,
+            "attributes": [
+                {
+                    "type": "name",
+                    "vals": ["mfa_user"],
+                },
+                {
+                    "type": "cn",
+                    "vals": ["mfa_user"],
+                },
+                {
+                    "type": "sAMAccountName",
+                    "vals": ["Test"],
+                },
+                {
+                    "type": "mail",
+                    "vals": [test_user_email],
+                },
+                {
+                    "type": "userPrincipalName",
+                    "vals": [test_user_email],
+                },
+                {
+                    "type": "displayName",
+                    "vals": ["MFA User"],
+                },
+                {
+                    "type": "memberOf",
+                    "vals": [
+                        "cn=mfa_group,cn=groups,dc=md,dc=test",
+                        "cn=domain admins,cn=groups,dc=md,dc=test",
+                    ],
+                },
+                {
+                    "type": "userAccountControl",
+                    "vals": ["512"],
+                },
+                {
+                    "type": "objectClass",
+                    "vals": ["organization", "top", "user"],
+                },
+            ],
+        },
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert data["resultCode"] == LDAPCodes.SUCCESS
+
+    auth = await unbound_http_client.post(
+        "auth/",
+        data={
+            "username": test_user_email,
+            "password": user_password,
+        },
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert auth.cookies.get("id")
+
+    return ProxyRequestModel(
+        principal=test_user_email,
+        ip="127.0.0.1",
+    ).model_dump()
---
+++ tests/test_api/test_shadow/test_router.py
@@ -0,0 +1,126 @@
+"""Test shadow api.
+
+Copyright (c) 2025 MultiFactor
+License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
+"""
+
+import pytest
+from fastapi import status
+from httpx import AsyncClient
+from sqlalchemy import delete, update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models import MFAFlags, NetworkPolicy
+
+from .conftest import ProxyRequestModel
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures('setup_session')
+async def test_shadow_api_non_existent_user(http_client: AsyncClient) -> None:
+    """Test shadow api with non-existent user."""
+    response = await http_client.post(
+        "/get/push/principal",
+        json=ProxyRequestModel(
+            principal='non-existent_user',
+            ip='127.0.0.1',
+        ).model_dump(),
+    )
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures('setup_session')
+async def test_shadow_api_without_network_policies(
+    http_client: AsyncClient,
+    adding_mfa_user_and_group: dict,
+    session: AsyncSession,
+) -> None:
+    """Test shadow api without network policy."""
+    await session.execute(delete(NetworkPolicy))
+
+    response = await http_client.post(
+        "/get/push/principal",
+        json=adding_mfa_user_and_group,
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures('setup_session')
+async def test_shadow_api_without_kerberos_protocol(
+    http_client: AsyncClient,
+    adding_mfa_user_and_group: dict,
+    session: AsyncSession,
+) -> None:
+    """Test shadow api without network policy with kerberos protocol."""
+    await session.execute(
+        update(NetworkPolicy)
+        .values({NetworkPolicy.is_kerberos: False}),
+    )
+
+    response = await http_client.post(
+        "/get/push/principal",
+        json=adding_mfa_user_and_group,
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures('setup_session')
+async def test_shadow_api_with_disable_mfa(
+    http_client: AsyncClient,
+    adding_mfa_user_and_group: dict,
+) -> None:
+    """Test shadow api with disable mfa."""
+    response = await http_client.post(
+        "/get/push/principal",
+        json=adding_mfa_user_and_group,
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures('setup_session')
+async def test_shadow_api_whitelist_without_user_group(
+    http_client: AsyncClient,
+    adding_mfa_user_and_group: dict,
+    session: AsyncSession,
+) -> None:
+    """Test shadow api with disable mfa."""
+    await session.execute(
+        update(NetworkPolicy)
+        .values({NetworkPolicy.mfa_status: MFAFlags.WHITELIST}),
+    )
+
+    response = await http_client.post(
+        "/get/push/principal",
+        json=adding_mfa_user_and_group,
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures('setup_session')
+async def test_shadow_api_enable_mfa(
+    http_client: AsyncClient,
+    adding_mfa_user_and_group: dict,
+    session: AsyncSession,
+) -> None:
+    """Test shadow api with disable mfa."""
+    await session.execute(
+        update(NetworkPolicy)
+        .values({NetworkPolicy.mfa_status: MFAFlags.ENABLED}),
+    )
+
+    response = await http_client.post(
+        "/get/push/principal",
+        json=adding_mfa_user_and_group,
+    )
+
+    assert response.status_code == status.HTTP_200_OK
