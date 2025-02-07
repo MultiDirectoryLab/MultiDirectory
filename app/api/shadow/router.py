17bb32e2cdb4a898d01b1484844a51ec8383e936
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-02-07 14:16:57 +0300

    refactor: rename url

--- app/api/shadow/router.py
+++ app/api/shadow/router.py
@@ -20,7 +20,7 @@ from models import MFAFlags, User
 shadow_router = APIRouter()
 
 
-@shadow_router.post("/mfa")
+@shadow_router.post("/mfa/push")
 @inject
 async def proxy_request(
     principal: Annotated[str, Body(embed=True)],
