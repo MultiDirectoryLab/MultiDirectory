ddef06b10327483270fafcfa4e5509f175ffc47f
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-02-24 12:22:32 +0300

    fix: isort

--- app/ldap_protocol/ldap_requests/abandon.py
+++ app/ldap_protocol/ldap_requests/abandon.py
@@ -6,6 +6,7 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 
 import asyncio
 from typing import AsyncGenerator, ClassVar
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ldap_protocol.asn1parser import ASN1Row
