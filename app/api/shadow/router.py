451daec6295320ecba99b591ab149ffce61ba458
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-02-24 13:32:29 +0300

    add: reset user password api for shadow_api

--- app/api/shadow/router.py
+++ app/api/shadow/router.py
@@ -14,9 +14,15 @@ from loguru import logger
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from ldap_protocol.kerberos import AbstractKadmin
 from ldap_protocol.multifactor import LDAPMultiFactorAPI, MultifactorAPI
 from ldap_protocol.policies.network_policy import get_user_network_policy
+from ldap_protocol.policies.password_policy import (
+    PasswordPolicySchema,
+    post_save_password_actions,
+)
 from models import MFAFlags, User
+from security import get_password_hash
 
 shadow_router = APIRouter()
 
@@ -79,3 +85,46 @@ async def proxy_request(
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
     )
+
+
+@shadow_router.post("/sync/password")
+@inject
+async def reset_password(
+    principal: Annotated[str, Body(embed=True)],
+    new_password: Annotated[str, Body(embed=True)],
+    session: FromDishka[AsyncSession],
+    kadmin: FromDishka[AbstractKadmin],
+) -> None:
+    """Reset user's (entry) password.
+
+    - **principal**: user upn
+    - **new_password**: password to set
+    \f
+    :param FromDishka[AsyncSession] session: db
+    :param FromDishka[AbstractKadmin] kadmin: kadmin api
+    :param Annotated[str, Body principal: reset target user
+    :param Annotated[str, Body new_password: new password for user
+    :raises HTTPException: 404 if user not found
+    :raises HTTPException: 422 if password not valid
+    :return None: None
+    """
+    query = select(User).filter(User.user_principal_name.ilike(principal))
+
+    user = await session.scalar(query)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+
+    policy = await PasswordPolicySchema.get_policy_settings(session, kadmin)
+    errors = await policy.validate_password_with_policy(new_password, user)
+
+    if errors:
+        raise HTTPException(
+            status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=errors,
+        )
+
+    user.password = get_password_hash(new_password)
+    await post_save_password_actions(user, session)
