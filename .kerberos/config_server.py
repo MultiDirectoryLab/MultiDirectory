9ce0975c6440e2f7d3bcd577861d1c5a4c5d664c
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-02-28 15:48:45 +0300

    refactor: remove default_policy handling and related functions

--- .kerberos/config_server.py
+++ .kerberos/config_server.py
@@ -223,7 +223,6 @@ class KAdminLocalManager(AbstractKRBManager):
         )
 
         princ = await self._get_raw_principal(name)
-        princ.policy = "default_policy"
         await self.loop.run_in_executor(
             self.pool,
             princ.commit,
@@ -332,48 +331,6 @@ class KAdminLocalManager(AbstractKRBManager):
         princ = await self._get_raw_principal(name)
         princ.pwexpire = "Now"
         await self.loop.run_in_executor(self.pool, princ.commit)
-
-
-async def create_update_default_policy(
-    minlife: int,
-    maxlife: int,
-    minlength: int,
-    minclasses: int,
-) -> None:
-    """Create or update default policy.
-
-    :param int minlife: minimum pw life
-    :param int maxlife: maximum pw life
-    :param int minlength: minimum pw length
-    :param int minclasses: cases of chars
-    """
-    proc = await asyncio.create_subprocess_shell(
-        "kadmin.local",
-        stdin=asyncio.subprocess.PIPE,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    msg = (
-        f'-maxlife "{maxlife} days" '
-        f'-minlife "{minlife} days" '
-        f"-minlength {minlength} "
-        f"-minclasses {minclasses} "
-        "default_policy"
-    )
-    _, stderr = await proc.communicate(("add_policy " + msg).encode("UTF-8"))
-    await proc.wait()
-
-    if "Principal or policy already exists" not in stderr.decode():
-        return
-
-    proc = await asyncio.create_subprocess_shell(
-        "kadmin.local",
-        stdin=asyncio.subprocess.PIPE,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    await proc.communicate(("modify_policy " + msg).encode("UTF-8"))
-    await proc.wait()
 
 
 @asynccontextmanager
@@ -522,8 +479,6 @@ async def run_setup_subtree(schema: ConfigSchema) -> None:
     with open("/etc/krb5kdc/kadm5.acl", "w") as f:
         f.write(f"*/admin@{schema.domain.upper()}        *\n")
 
-    await create_update_default_policy(0, 1, 2, 2)
-
 
 @setup_router.post("/reset")
 async def reset_setup() -> None:
@@ -549,27 +504,6 @@ async def add_princ(
     :param Annotated[str, Body password: principal password
     """
     await kadmin.add_princ(name, password)
-
-
-@principal_router.post("/password_policy")
-async def create_or_update_default_pw_policy(
-    minlife: Annotated[int, Body()],
-    maxlife: Annotated[int, Body()],
-    minlength: Annotated[int, Body()],
-    minclasses: Annotated[int, Body()],
-) -> None:
-    """Add principal.
-
-    :param Annotated[AbstractKRBManager, Depends kadmin: kadmin abstract
-    :param Annotated[str, Body name: principal name
-    :param Annotated[str, Body password: principal password
-    """
-    await create_update_default_policy(
-        minlife,
-        maxlife,
-        minlength,
-        minclasses,
-    )
 
 
 @principal_router.get("")
