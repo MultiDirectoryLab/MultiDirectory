2d517d4f34acf56c17e3ad3939ecc3b8d39c3682
Naksen <khannanov.rus@gmail.com>
2025-03-26 12:36:43 +0300

    refactor: update cert dumping process

--- app/extra/dump_acme_certs.py
+++ app/extra/dump_acme_certs.py
@@ -7,56 +7,43 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 import base64
 import json
 import os
-import shutil
 
 from loguru import logger
 
 
-def write_with_backup(filepath: str, content: str) -> None:
-    """Backup the file and write the content."""
-    if os.path.exists(filepath):
-        shutil.copy(filepath, f"{filepath}.bak")
-        logger.info(f"Backup created: {filepath}.bak")
+def dump_acme_cert() -> None:
+    """Dump Let's Encrypt certificate from the `acme.json` file."""
+    if not os.path.exists("/certs/acme.json"):
+        logger.warning("Cannot load ACME file for MultiDirectory")
+        return
 
-    with open(filepath, "w") as file:
-        file.write(content)
+    if os.path.exists("/certs/cert.pem") and os.path.exists(
+        "/certs/privkey.pem"
+    ):
+        logger.info("Cert already exists")
+        return
 
+    try:
+        with open("/certs/acme.json") as certfile:
+            data = json.load(certfile)
 
-acme_exc = SystemError(
-    "Let's Encrypt certificate not found. The `acme.json` "
-    "file might not have been generated or lacks certificate details. "
-    "This can also occur if the certificate failed to generate "
-    "for localhost, as Let's Encrypt only issues "
-    "certificates for public domains. "
-    "Try deleting and recreating the `acme.json` file, "
-    "or consider using a self-signed certificate "
-    "for local environments or closed networks."
-)
+        domain = data["md-resolver"]["Certificates"][0]["domain"]["main"]
+        cert = data["md-resolver"]["Certificates"][0]["certificate"]
+        key = data["md-resolver"]["Certificates"][0]["key"]
+    except (KeyError, IndexError, TypeError, json.JSONDecodeError):
+        logger.warning("Cannot load TLS cert for MultiDirectory")
+        return
 
-if not os.path.exists("/certs/acme.json"):
-    logger.critical("Cannot load ACME file for MultiDirectory")
-    raise acme_exc
+    else:
+        logger.info(f"Loaded cert for {domain}")
 
-try:
-    with open("/certs/acme.json") as certfile:
-        data = json.load(certfile)
+        cert = base64.b64decode(cert.encode("ascii")).decode()
+        key = base64.b64decode(key.encode("ascii")).decode()
 
-    domain = data["md-resolver"]["Certificates"][0]["domain"]["main"]
-    cert = data["md-resolver"]["Certificates"][0]["certificate"]
-    key = data["md-resolver"]["Certificates"][0]["key"]
-except (KeyError, IndexError, TypeError, json.JSONDecodeError) as err:
-    logger.critical("Cannot load TLS cert for MultiDirectory")
-    raise acme_exc from err
+        with open("/certs/cert.pem", "w") as f:
+            f.write(cert)
 
-else:
-    logger.info(f"Saved cert for {domain}")
+        with open("/certs/privkey.pem", "w") as f:
+            f.write(key)
 
-    cert = base64.b64decode(cert.encode("ascii")).decode()
-    key = base64.b64decode(key.encode("ascii")).decode()
-
-    write_with_backup("/certs/cert.pem", cert)
-    write_with_backup("/certs/privkey.pem", key)
-
-    logger.info(
-        "Let's Encrypt cert saved to /certs/cert.pem, /certs/privkey.pem"
-    )
+        logger.info("Cert dumped")
