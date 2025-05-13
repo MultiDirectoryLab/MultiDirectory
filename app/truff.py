0771a9859075abf3e9fd71a7eb7417dd1bf0fb28
Milov Dmitriy <milovdd@mail.ru>
2025-05-13 18:42:39 +0300

    tests

--- app/truff.py
+++ app/truff.py
@@ -13,15 +13,7 @@ from typing import AsyncIterator, Callable
 
 import uvicorn
 import uvloop
-from alembic.config import Config, command
-from dishka import make_async_container
-from dishka.integrations.fastapi import setup_dishka
-from dns.exception import DNSException
-from fastapi import FastAPI, Request, Response
-from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import exc as sa_exc
-
-from api import access_policy_router, auth_router
+from alembic.config import Config
 
 
 async def aboba() -> None:
@@ -38,14 +30,3 @@ async def aboba() -> None:
     logger.info(uvicorn)
     logger.info(uvloop)
     logger.info(Config)
-    logger.info(command)
-    logger.info(make_async_container)
-    logger.info(setup_dishka)
-    logger.info(DNSException)
-    logger.info(FastAPI)
-    logger.info(Request)
-    logger.info(Response)
-    logger.info(CORSMiddleware)
-    logger.info(sa_exc)
-    logger.info(access_policy_router)
-    logger.info(auth_router)
