8b9d4f3a66e7030254ed855ac1a36d0722716b36
Milov Dmitriy <milovdd@mail.ru>
2025-05-13 18:53:08 +0300

    tests

--- app/truff.py
+++ app/truff.py
@@ -7,8 +7,6 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 from typing import AsyncIterator, Callable
 
 import uvicorn
-import uvloop
-from alembic.config import Config
 
 
 async def aboba() -> None:
@@ -18,5 +16,3 @@ async def aboba() -> None:
     logger.info(AsyncIterator)
     logger.info(Callable)
     logger.info(uvicorn)
-    logger.info(uvloop)
-    logger.info(Config)
