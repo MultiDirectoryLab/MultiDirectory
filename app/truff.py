eae84555d9419dcd18ca191cace7d1fc4cf7a9e8
Milov Dmitriy <milovdd@mail.ru>
2025-05-13 18:39:10 +0300

    tests

--- app/truff.py
+++ app/truff.py
@@ -21,31 +21,7 @@ from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import exc as sa_exc
 
-from api import (
-    access_policy_router,
-    auth_router,
-    dns_router,
-    entry_router,
-    krb5_router,
-    mfa_router,
-    network_router,
-    pwd_router,
-    session_router,
-    shadow_router,
-)
-from api.exception_handlers import handle_db_connect_error, handle_dns_error
-from config import Settings
-from extra.dump_acme_certs import dump_acme_cert
-from ioc import (
-    HTTPProvider,
-    LDAPServerProvider,
-    MainProvider,
-    MFACredsProvider,
-    MFAProvider,
-)
-from ldap_protocol.dns import DNSConnectionError
-from ldap_protocol.server import PoolClientHandler
-from schedule import scheduler
+from api import access_policy_router, auth_router
 
 
 async def aboba() -> None:
@@ -73,23 +49,3 @@ async def aboba() -> None:
     logger.info(sa_exc)
     logger.info(access_policy_router)
     logger.info(auth_router)
-    logger.info(dns_router)
-    logger.info(entry_router)
-    logger.info(krb5_router)
-    logger.info(mfa_router)
-    logger.info(network_router)
-    logger.info(pwd_router)
-    logger.info(session_router)
-    logger.info(shadow_router)
-    logger.info(handle_db_connect_error)
-    logger.info(handle_dns_error)
-    logger.info(Settings)
-    logger.info(dump_acme_cert)
-    logger.info(HTTPProvider)
-    logger.info(LDAPServerProvider)
-    logger.info(MainProvider)
-    logger.info(MFACredsProvider)
-    logger.info(MFAProvider)
-    logger.info(DNSConnectionError)
-    logger.info(PoolClientHandler)
-    logger.info(scheduler)
