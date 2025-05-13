569010081a662dea1cfb6139032184cce2a621a4
Milov Dmitriy <milovdd@mail.ru>
2025-05-13 18:56:08 +0300

    tests

--- app/truff.py
+++ app/truff.py
@@ -4,15 +4,13 @@ Copyright (c) 2024 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
-from typing import AsyncIterator, Callable
-
-import uvicorn
+import uvloop
+from alembic.config import Config
 
 
 async def aboba() -> None:
     """Aboba function."""
     from loguru import logger
 
-    logger.info(AsyncIterator)
-    logger.info(Callable)
-    logger.info(uvicorn)
+    logger.info(uvloop)
+    logger.info(Config)
