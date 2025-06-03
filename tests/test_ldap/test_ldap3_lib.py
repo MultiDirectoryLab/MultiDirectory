6253de8fde5990fbca38a5bbc3253aa386d38b33
m.shvets <m89258262994@example.com>
2025-06-03 09:25:34 +0300

    refactor: replaced ldap3 with aioldap3

--- tests/test_ldap/test_ldap3_lib.py
+++ tests/test_ldap/test_ldap3_lib.py
@@ -4,12 +4,9 @@ Copyright (c) 2024 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
-from asyncio import BaseEventLoop
-from functools import partial
-
 import pytest
-from ldap3 import Connection
 
+from aioldap3 import LDAPConnection
 from tests.conftest import TestCreds
 
 
@@ -17,53 +14,37 @@ from tests.conftest import TestCreds
 @pytest.mark.usefixtures("setup_session")
 @pytest.mark.usefixtures("session")
 async def test_ldap3_search(
-    ldap_client: Connection,
-    event_loop: BaseEventLoop,
+    ldap_client: LDAPConnection,
     creds: TestCreds,
 ) -> None:
     """Test ldap3 search."""
-    await event_loop.run_in_executor(
-        None,
-        partial(ldap_client.rebind, user=creds.un, password=creds.pw),
-    )
+    await ldap_client.bind(creds.un, creds.pw)
 
-    result = await event_loop.run_in_executor(
-        None,
-        partial(
-            ldap_client.search,
-            "dc=md,dc=test",
-            "(objectclass=*)",
-        ),
+    result = await ldap_client.search(
+        "dc=md,dc=test",
+        "(objectclass=*)",
     )
 
     assert result
-    assert ldap_client.entries
+    assert result.entries
 
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("setup_session")
 @pytest.mark.usefixtures("session")
 async def test_ldap3_search_memberof(
-    ldap_client: Connection,
-    event_loop: BaseEventLoop,
+    ldap_client: LDAPConnection,
     creds: TestCreds,
 ) -> None:
     """Test ldap3 search memberof."""
     member = "cn=user1,ou=moscow,ou=russia,ou=users,dc=md,dc=test"
-    await event_loop.run_in_executor(
-        None,
-        partial(ldap_client.rebind, user=creds.un, password=creds.pw),
-    )
+    await ldap_client.bind(creds.un, creds.pw)
 
-    result = await event_loop.run_in_executor(
-        None,
-        partial(
-            ldap_client.search,
-            "dc=md,dc=test",
-            "(memberOf=cn=developers,cn=groups,dc=md,dc=test)",
-        ),
+    result = await ldap_client.search(
+        "dc=md,dc=test",
+        "(memberOf=cn=developers,cn=groups,dc=md,dc=test)",
     )
 
     assert result
-    assert ldap_client.entries
-    assert ldap_client.entries[0].entry_dn == member
+    assert result.entries
+    assert result.entries[0]["dn"] == member
