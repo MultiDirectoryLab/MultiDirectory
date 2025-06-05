0610c8cb7aa66c7fc2305f9ba225919b473991a7
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-06-05 15:39:17 +0300

    refactor: replace RedisClient with RedisAuditDAO in LDAP request handling and enhance event processing logic

--- app/ldap_protocol/ldap_requests/base.py
+++ app/ldap_protocol/ldap_requests/base.py
@@ -4,7 +4,9 @@ Copyright (c) 2024 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
+import asyncio
 from abc import ABC, abstractmethod
+from ipaddress import IPv4Address, IPv6Address
 from typing import TYPE_CHECKING, AsyncGenerator, Callable, ClassVar, Protocol
 
 from dishka import AsyncContainer
@@ -15,6 +17,10 @@ from config import Settings
 from ldap_protocol.dependency import resolve_deps
 from ldap_protocol.dialogue import LDAPSession
 from ldap_protocol.ldap_responses import BaseResponse, LDAPResult
+from ldap_protocol.policies.audit_policy import (
+    AuditEventBuilder,
+    RedisAuditDAO,
+)
 from ldap_protocol.utils.helpers import get_class_name
 from models import Directory
 
@@ -40,6 +46,7 @@ if TYPE_CHECKING:
         async def _handle_api(
             self,
             container: AsyncContainer,
+            ip: IPv4Address | IPv6Address,
         ) -> list[BaseResponse] | BaseResponse: ...
 else:
 
@@ -70,19 +77,33 @@ class BaseRequest(ABC, _APIProtocol, BaseModel):
     def get_directory_attrs(self, directory: Directory) -> dict:
         """Get directory attrs."""
         attributes: dict[str, list] = {}
-
+        obj_classes = []
         for attr in directory.attributes:
             attr_name = attr.name.lower()
+            if attr_name == "objectclass":
+                obj_classes.append(attr.value)
+
             if attr_name not in attributes:
                 attributes[attr_name] = []
 
             attributes[attr_name].append(attr.value)
+
+        if "group" in obj_classes or "user" in obj_classes:
+            attributes["memberof"] = []
+            for group in directory.groups:
+                attributes["memberof"].append(group.directory.path_dn)
+
+        if "group" in obj_classes and directory.group:
+            attributes["member"] = []
+            for member in directory.group.members:
+                attributes["member"].append(member.path_dn)
 
         return attributes
 
     async def _handle_api(
         self,
         container: AsyncContainer,
+        ip: IPv4Address | IPv6Address,
     ) -> list[BaseResponse]:
         """Hanlde response with api user.
 
@@ -95,7 +116,7 @@ class BaseRequest(ABC, _APIProtocol, BaseModel):
 
         ldap_session = await container.get(LDAPSession)
         settings = await container.get(Settings)
-
+        redis_client: RedisAuditDAO = await container.get(RedisAuditDAO)
         un = getattr(ldap_session.user, "user_principal_name", "ANONYMOUS")
 
         if settings.DEBUG:
@@ -116,8 +137,26 @@ class BaseRequest(ABC, _APIProtocol, BaseModel):
             for response in responses:
                 log_api.info(f"{get_class_name(response)}[{un}]")
 
+        if await redis_client.is_event_processing_enabled(self.PROTOCOL_OP):
+            event = AuditEventBuilder.from_ldap_request(
+                request=self,
+                responses=responses,
+                username=un,
+                ip=ip,
+                protocol="API_LDAP",
+                settings=settings,
+                help_data=self.get_event_data(),
+            )
+            asyncio.create_task(
+                redis_client.add_audit_event(
+                    event=event,
+                    stream_name=settings.EVENT_STREAM_NAME,
+                ),
+            )
         return responses
 
-    async def handle_api(self, container: AsyncContainer) -> LDAPResult:
+    async def handle_api(
+        self, container: AsyncContainer, ip: IPv4Address | IPv6Address
+    ) -> LDAPResult:
         """Get single response."""
-        return (await self._handle_api(container))[0]  # type: ignore
+        return (await self._handle_api(container, ip))[0]  # type: ignore
