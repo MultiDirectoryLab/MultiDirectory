b00ce71e0b154c65a48eb58cbae67b0793162c2c
iyashnov <x13777x@gmail.com>
2025-06-20 12:27:46 +0300

    refactor: update entrypoint and Dockerfile for consistent virtual environment usage

--- .dns/entrypoint.sh
+++ .dns/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/local/bin/docker-entrypoint.sh &
-/venvs/bin/python3.12 -m uvicorn --factory dns_api:create_app --host 0.0.0.0 --reload &
+/venvs/.venv/bin/python3.12 -m uvicorn --factory dns_api:create_app --host 0.0.0.0 --reload &
 
 wait -n
 
