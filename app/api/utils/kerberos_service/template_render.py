ea8a622ce2ca2c2075f95bc03580e8ec0bf90903
m.shvets <m89258262994@example.com>
2025-07-01 17:45:12 +0300

    refactor: streamline variable assignments in LDAPStructureManager and KerberosService classes

--- app/api/utils/kerberos_service/template_render.py
+++ app/api/utils/kerberos_service/template_render.py
@@ -4,7 +4,7 @@ Copyright (c) 2024 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
-from fastapi.templating import Jinja2Templates
+import jinja2
 
 from .typing import KDCContext
 
@@ -16,7 +16,7 @@ class TemplateRenderer:
     using provided Jinja2 templates and context data.
     """
 
-    def __init__(self, templates: Jinja2Templates) -> None:
+    def __init__(self, templates: jinja2.Environment) -> None:
         """Initialize TemplateRenderer with Jinja2 templates.
 
         :param templates: Jinja2 environment or template loader.
