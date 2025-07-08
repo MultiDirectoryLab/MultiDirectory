6f56dee25e72a7fafa356caf743476feeacc781b
Milov Dmitriy <milovdd@mail.ru>
2025-07-08 10:55:42 +0300

    add: modify_dn: attach entity to dir

--- app/ldap_protocol/utils/queries.py
+++ app/ldap_protocol/utils/queries.py
@@ -329,20 +329,18 @@ async def add_lock_and_expire_attributes(
     """
     now_with_tz = datetime.now(tz=tz)
     absolute_date = int(time.mktime(now_with_tz.timetuple()) / 86400)
-    session.add_all(
-        [
-            Attribute(
-                name="nsAccountLock",
-                value="true",
-                directory=directory,
-            ),
-            Attribute(
-                name="shadowExpire",
-                value=str(absolute_date),
-                directory=directory,
-            ),
-        ]
-    )
+    session.add_all([
+        Attribute(
+            name="nsAccountLock",
+            value="true",
+            directory=directory,
+        ),
+        Attribute(
+            name="shadowExpire",
+            value=str(absolute_date),
+            directory=directory,
+        ),
+    ])
 
 
 async def get_principal_directory(
