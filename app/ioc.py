4ba74367862660f2c1eb4af3481692fea04eb7f2
m.shvets <m89258262994@example.com>
2025-07-15 10:52:14 +0300

    feat: enhance MFA management with MFAManagerFastAPIAdapter
    - Introduced MFAManagerFastAPIAdapter to facilitate integration of MFAManager with FastAPI, improving the handling of multi-factor authentication.
    - Updated dependency injection in the MainProvider and test configurations to utilize the new adapter, ensuring consistent MFA management across the application.
    - Refactored authentication routes to leverage the adapter, streamlining the MFA setup and callback processes.

--- app/ioc.py
+++ app/ioc.py
@@ -19,7 +19,7 @@ from api.utils.auth_manager import (
     IdentityManager,
     IdentityManagerFastAPIAdapter,
 )
-from api.utils.mfa_manager import MFAManager
+from api.utils.mfa_manager import MFAManager, MFAManagerFastAPIAdapter
 from config import Settings
 from ldap_protocol.dialogue import LDAPSession
 from ldap_protocol.dns import (
@@ -78,7 +78,7 @@ class MainProvider(Provider):
             yield session
             await session.commit()
 
-    @provide(scope=Scope.SESSION)
+    @provide(scope=Scope.APP)
     async def get_krb_class(
         self,
         session_maker: async_sessionmaker[AsyncSession],
@@ -244,7 +244,18 @@ class HTTPProvider(Provider):
             IdentityManager(session, settings, mfa_api, storage)
         )
 
-    mfa_manager = provide(provides=MFAManager, scope=Scope.REQUEST)
+    @provide(scope=Scope.REQUEST)
+    def get_mfa_manager(
+        self,
+        session: AsyncSession,
+        settings: Settings,
+        storage: SessionStorage,
+        mfa_api: MultifactorAPI,
+    ) -> MFAManagerFastAPIAdapter:
+        """Get MFA manager."""
+        return MFAManagerFastAPIAdapter(
+            MFAManager(session, settings, storage, mfa_api)
+        )
 
 
 class LDAPServerProvider(Provider):
