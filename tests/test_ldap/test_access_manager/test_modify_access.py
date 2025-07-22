29792c7b02153c012690c98364d852520d9e7747
Naksen <khannanov.rus@gmail.com>
2025-07-22 14:32:36 +0300

    test: update test description for ACE checks from search to modify access

--- tests/test_ldap/test_access_manager/test_modify_access.py
+++ tests/test_ldap/test_access_manager/test_modify_access.py
@@ -1,4 +1,4 @@
-"""Test ACE checks for search access.
+"""Test ACE checks for modify access.
 
 Copyright (c) 2025 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
