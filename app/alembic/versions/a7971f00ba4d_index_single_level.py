02d2b59122de2b4a663c696fb933d0533dd2fa60
Mastermind-U <rex49513@gmail.com>
2025-07-22 17:45:01 +0300

    add: indexes to Directory and Attributes tables for improved query performance

---
+++ app/alembic/versions/a7971f00ba4d_index_single_level.py
@@ -0,0 +1,47 @@
+"""index_single_level.
+
+Revision ID: a7971f00ba4d
+Revises: 35d1542d2505
+Create Date: 2025-07-22 11:13:48.397808
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "a7971f00ba4d"
+down_revision = "35d1542d2505"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Create index for Directory depth field."""
+    op.execute(
+        sa.text(
+            "CREATE INDEX IF NOT EXISTS idx_Directory_depth_hash "
+            'ON "Directory" '
+            "USING hash (depth);"
+        )
+    )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_composite_attributes_directory_id_name "
+            'ON "Attributes" ("directoryId", lower("name"));'
+        )
+    )
+
+
+def downgrade() -> None:
+    """Dg."""
+    op.drop_index(
+        op.f("idx_Directory_depth_hash"),
+        table_name="Directory",
+        if_exists=True,
+    )
+    op.drop_index(
+        "idx_composite_attributes_directory_id_name",
+        table_name="Attributes",
+        if_exists=True,
+    )
