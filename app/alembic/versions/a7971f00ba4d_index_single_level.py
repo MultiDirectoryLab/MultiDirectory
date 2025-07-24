29bcdec8a39c340d3bd82a4484d4005c7d7cf233
Mastermind-U <rex49513@gmail.com>
2025-07-24 14:29:03 +0300

    fix: update index creation and removal for Users, Directory, and Attributes tables

--- app/alembic/versions/a7971f00ba4d_index_single_level.py
+++ app/alembic/versions/a7971f00ba4d_index_single_level.py
@@ -20,7 +20,43 @@ def upgrade() -> None:
     """Create index for Directory depth field."""
     op.execute(
         sa.text(
-            "CREATE INDEX IF NOT EXISTS idx_Directory_depth_hash "
+            "CREATE INDEX idx_User_san_gin "
+            'ON "Users" USING gin ("sAMAccountName" gin_trgm_ops);'
+        )
+    )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_User_upn_gin "
+            'ON "Users" USING gin ("userPrincipalName" gin_trgm_ops);'
+        )
+    )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_User_display_name_gin "
+            'ON "Users" USING gin ("displayName" gin_trgm_ops);'
+        )
+    )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_user_hash_dir_id "
+            'ON "Users" USING hash ("directoryId");'
+        )
+    )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_entity_type_dir_id "
+            'ON "Directory" USING hash ("entity_type_id");'
+        )
+    )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_group_dir_id "
+            'ON "Groups" USING hash ("directoryId");'
+        )
+    )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_Directory_depth_hash "
             'ON "Directory" '
             "USING hash (depth);"
         )
@@ -31,17 +67,30 @@ def upgrade() -> None:
             'ON "Attributes" ("directoryId", lower("name"));'
         )
     )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_attributes_value "
+            'ON "Attributes" USING gin ("value" gin_trgm_ops);'
+        )
+    )
+    op.execute(
+        sa.text(
+            "CREATE INDEX idx_attributes_name_value_trgm "
+            'ON "Attributes" USING gin '
+            '("name" gin_trgm_ops, "value" gin_trgm_ops);'
+        )
+    )
 
 
 def downgrade() -> None:
     """Remove indexes for Directory depth field and Attributes table."""
-    op.drop_index(
-        op.f("idx_Directory_depth_hash"),
-        table_name="Directory",
-        if_exists=True,
-    )
-    op.drop_index(
-        "idx_composite_attributes_directory_id_name",
-        table_name="Attributes",
-        if_exists=True,
-    )
+    op.drop_index("idx_User_san_gin", "Users")
+    op.drop_index("idx_User_upn_gin", "Users")
+    op.drop_index("idx_User_display_name_gin", "Users")
+    op.drop_index("idx_user_hash_dir_id", "Users")
+    op.drop_index("idx_entity_type_dir_id", "Directory")
+    op.drop_index("idx_group_dir_id", "Groups")
+    op.drop_index("idx_Directory_depth_hash", "Directory")
+    op.drop_index("idx_composite_attributes_directory_id_name", "Attributes")
+    op.drop_index("idx_attributes_value", "Attributes")
+    op.drop_index("idx_attributes_name_value_trgm", "Attributes")
