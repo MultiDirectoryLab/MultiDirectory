58784fbdc0ea7999da3e810f03dd6fc424f5b9ab
Mastermind-U <rex49513@gmail.com>
2025-07-24 14:43:16 +0300

    fix: standardize index creation syntax in alembic migration

--- app/alembic/versions/a7971f00ba4d_index_single_level.py
+++ app/alembic/versions/a7971f00ba4d_index_single_level.py
@@ -20,62 +20,62 @@ def upgrade() -> None:
     """Create index for Directory depth field."""
     op.execute(
         sa.text(
-            "CREATE INDEX idx_User_san_gin "
+            'CREATE INDEX "idx_User_san_gin" '
             'ON "Users" USING gin ("sAMAccountName" gin_trgm_ops);'
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_User_upn_gin "
+            'CREATE INDEX "idx_User_upn_gin" '
             'ON "Users" USING gin ("userPrincipalName" gin_trgm_ops);'
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_User_display_name_gin "
+            'CREATE INDEX "idx_User_display_name_gin" '
             'ON "Users" USING gin ("displayName" gin_trgm_ops);'
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_user_hash_dir_id "
+            'CREATE INDEX "idx_user_hash_dir_id" '
             'ON "Users" USING hash ("directoryId");'
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_entity_type_dir_id "
+            'CREATE INDEX "idx_entity_type_dir_id" '
             'ON "Directory" USING hash ("entity_type_id");'
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_group_dir_id "
+            'CREATE INDEX "idx_group_dir_id" '
             'ON "Groups" USING hash ("directoryId");'
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_Directory_depth_hash "
+            'CREATE INDEX "idx_Directory_depth_hash" '
             'ON "Directory" '
             "USING hash (depth);"
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_composite_attributes_directory_id_name "
+            'CREATE INDEX "idx_composite_attributes_directory_id_name" '
             'ON "Attributes" ("directoryId", lower("name"));'
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_attributes_value "
+            'CREATE INDEX "idx_attributes_value" '
             'ON "Attributes" USING gin ("value" gin_trgm_ops);'
         )
     )
     op.execute(
         sa.text(
-            "CREATE INDEX idx_attributes_name_value_trgm "
+            'CREATE INDEX "idx_attributes_name_value_trgm" '
             'ON "Attributes" USING gin '
             '("name" gin_trgm_ops, "value" gin_trgm_ops);'
         )
