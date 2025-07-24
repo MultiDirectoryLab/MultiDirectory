21280b680c25b0a0578b907572a1848653b6fae9
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-07-24 17:00:50 +0300

    refactor: rename AuditDAO to AuditPoliciesDAO and update references across the codebase

--- app/ioc.py
+++ app/ioc.py
@@ -52,7 +52,7 @@ from ldap_protocol.multifactor import (
     MultifactorAPI,
     get_creds,
 )
-from ldap_protocol.policies.audit_policies import AuditDAO
+from ldap_protocol.policies.audit.policies_dao import AuditPoliciesDAO
 from ldap_protocol.roles.access_manager import AccessManager
 from ldap_protocol.roles.role_dao import RoleDAO
 from ldap_protocol.roles.role_use_case import RoleUseCase
@@ -196,7 +196,7 @@ class MainProvider(Provider):
         yield SessionStorageClient(client)
         await client.aclose()
 
-    audit_dao = provide(AuditDAO, scope=Scope.REQUEST)
+    audit_dao = provide(AuditPoliciesDAO, scope=Scope.REQUEST)
 
     @provide(scope=Scope.APP)
     async def get_session_storage(
