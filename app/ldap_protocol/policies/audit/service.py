31748c89ab54c9c150c4d9c84a63c27260877345
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-07-28 17:43:53 +0300

    add: enhance error handling and documentation for audit policy and destination updates

--- app/ldap_protocol/policies/audit/service.py
+++ app/ldap_protocol/policies/audit/service.py
@@ -4,7 +4,10 @@ Copyright (c) 2025 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
+from sqlalchemy.exc import IntegrityError
+
 from .destination_dao import AuditDestinationDAO
+from .exception import AuditAlreadyExistsError
 from .policies_dao import AuditPoliciesDAO
 from .schemas import (
     AuditDestinationSchema,
@@ -38,14 +41,28 @@ class AuditService:
         policy_id: int,
         policy: AuditPolicySchemaRequest,
     ) -> AuditPolicySchema:
-        """Update an existing audit policy."""
+        """Update an existing audit policy.
+
+        Args:
+            policy_id (int): The ID of the policy to update.
+            policy (AuditPolicySchemaRequest): The new policy data.
+
+        Raises:
+            AuditNotFoundError: If the policy with the given ID does not exist.
+            AuditAlreadyExistsError: If the policy already exists.
+
+        """
         existing_policy = await self._policy_dao.get_policy_by_id(policy_id)
-        await self._policy_dao.update_policy(
-            existing_policy,
-            policy.id,
-            policy.name,
-            policy.is_enabled,
-        )
+        try:
+            await self._policy_dao.update_policy(
+                existing_policy,
+                policy.id,
+                policy.name,
+                policy.is_enabled,
+            )
+        except IntegrityError:
+            raise AuditAlreadyExistsError("Audit policy already exists")
+
         new_policy = await self._policy_dao.get_policy_by_id(policy.id)
         return AuditPolicySchema.model_validate(new_policy.__dict__)
 
@@ -60,15 +77,29 @@ class AuditService:
         self,
         destination: AuditDestinationSchema,
     ) -> AuditDestinationSchema:
-        """Create a new audit destination."""
-        created_destination = await self._destination_dao.create_destination(
-            destination.name,
-            destination.service_type,
-            destination.host,
-            destination.port,
-            destination.protocol,
-            destination.is_enabled,
-        )
+        """Create a new audit destination.
+
+        Args:
+            destination (AuditDestinationSchema): Destination data to create.
+
+        Raises:
+            AuditAlreadyExistsError: If the destination already exists.
+
+        """
+        try:
+            created_destination = (
+                await self._destination_dao.create_destination(
+                    destination.name,
+                    destination.service_type,
+                    destination.host,
+                    destination.port,
+                    destination.protocol,
+                    destination.is_enabled,
+                )
+            )
+        except IntegrityError:
+            raise AuditAlreadyExistsError("Audit destination already exists")
+
         return AuditDestinationSchema.model_validate(
             created_destination.__dict__
         )
@@ -78,19 +109,33 @@ class AuditService:
         destination_id: int,
         destination: AuditDestinationSchemaRequest,
     ) -> AuditDestinationSchema:
-        """Update an existing audit destination."""
+        """Update an existing audit destination.
+
+        Args:
+            destination_id (int): The ID of the destination to update.
+            destination (AuditDestinationSchemaRequest): New destination data.
+
+        Raises:
+            AuditNotFoundError: If the destination with ID does not exist.
+            AuditAlreadyExistsError: If the destination already exists.
+
+        """
         existing_destination = (
             await self._destination_dao.get_destination_by_id(destination_id)
         )
-        await self._destination_dao.update_destination(
-            existing_destination,
-            destination.name,
-            destination.service_type,
-            destination.host,
-            destination.port,
-            destination.protocol,
-            destination.is_enabled,
-        )
+        try:
+            await self._destination_dao.update_destination(
+                existing_destination,
+                destination.name,
+                destination.service_type,
+                destination.host,
+                destination.port,
+                destination.protocol,
+                destination.is_enabled,
+            )
+        except IntegrityError:
+            raise AuditAlreadyExistsError("Audit destination already exists")
+
         updated_destination = (
             await self._destination_dao.get_destination_by_id(
                 existing_destination.id
@@ -104,7 +149,15 @@ class AuditService:
         self,
         destination_id: int,
     ) -> None:
-        """Delete an audit destination."""
+        """Delete an audit destination.
+
+        Args:
+            destination_id (int): The ID of the destination to delete.
+
+        Raises:
+            AuditNotFoundError: If the destination with ID does not exist.
+
+        """
         destination = await self._destination_dao.get_destination_by_id(
             destination_id
         )
