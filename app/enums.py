17a86fbb51896faabecda63d27871a951ea04fd6
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-07-29 15:04:53 +0300

    refactor: reorganize enums and import statements for clarity and consistency

--- app/enums.py
+++ app/enums.py
@@ -4,7 +4,7 @@ Copyright (c) 2025 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
-from enum import Enum, IntEnum, StrEnum
+from enum import Enum, IntEnum
 
 
 class AceType(IntEnum):
@@ -31,29 +31,3 @@ class MFAFlags(int, Enum):
     DISABLED = 0
     ENABLED = 1
     WHITELIST = 2
-
-
-class AuditSeverity(IntEnum):
-    """Audit policy severity."""
-
-    EMERGENCY = 0
-    ALERT = 1
-    CRITICAL = 2
-    ERROR = 3
-    WARNING = 4
-    NOTICE = 5
-    INFO = 6
-    DEBUG = 7
-
-
-class AuditDestinationProtocolType(StrEnum):
-    """Audit destination protocol type."""
-
-    UDP = "udp"
-    TCP = "tcp"
-
-
-class AuditDestinationServiceType(StrEnum):
-    """Audit destination type."""
-
-    SYSLOG = "syslog"
