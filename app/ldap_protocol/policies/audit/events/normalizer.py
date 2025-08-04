d9e15ab330139b8be5fb3692e53c33d1b25d3c31
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-08-04 13:13:46 +0300

    add: modules for processing and normalizing audit events

---
+++ app/ldap_protocol/policies/audit/events/normalizer.py
@@ -0,0 +1,115 @@
+"""Normalize audit events.
+
+Copyright (c) 2025 MultiFactor
+License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
+"""
+
+from ldap_protocol.objects import OperationEvent
+from models import AuditPolicyTrigger
+
+from .dataclasses import NormalizedAuditEvent, RawAuditEvent
+
+
+class AuditEventNormalizer:
+    """Interactor for normalizing audit events."""
+
+    event_data: RawAuditEvent
+    trigger: AuditPolicyTrigger
+
+    def __init__(
+        self,
+        event_data: RawAuditEvent,
+        trigger: AuditPolicyTrigger,
+    ) -> None:
+        """Initialize normalizer with event data and trigger."""
+        self.event_data = event_data
+        self.trigger = trigger
+
+    def _get_common_fields(self) -> dict:
+        """Extract common fields from event data and trigger."""
+        protocol = "API" if "API" in self.event_data.protocol else "LDAP"
+        if self.trigger.operation_code in {
+            OperationEvent.KERBEROS_AUTH,
+            OperationEvent.CHANGE_PASSWORD_KERBEROS,
+        }:
+            protocol = "KERBEROS"
+
+        return {
+            "username": self.event_data.username,
+            "source_ip": str(self.event_data.source_ip),
+            "dest_port": self.event_data.dest_port,
+            "timestamp": self.event_data.timestamp,
+            "hostname": self.event_data.hostname,
+            "protocol": protocol,
+            "event_type": self.trigger.audit_policy.name,
+            "severity": self.trigger.audit_policy.severity,
+            "policy_id": self.trigger.audit_policy.id,
+            "operation_success": self.trigger.is_operation_success,
+            "service_name": self.event_data.service_name,
+        }
+
+    def _enrich_ldap_details(self, details: dict) -> None:
+        """Add LDAP-specific details to event details."""
+        if self.event_data.request_code == OperationEvent.MODIFY:
+            details["target_dn"] = self.event_data.request["object"]
+
+            if not self.trigger.additional_info:
+                raise ValueError(
+                    "Modify operation trigger must have additional_info",
+                )
+
+            if not self.trigger.additional_info["change_attributes"]:
+                return
+
+            change_attribute = self.trigger.additional_info[
+                "change_attributes"
+            ][0]
+            if change_attribute in {"member", "memberof"}:
+                first_value = set(
+                    self.event_data.context["before_attrs"][change_attribute],
+                )
+                second_value = set(
+                    self.event_data.context["after_attrs"][change_attribute],
+                )
+                if not first_value - second_value:
+                    details["diff_groups"] = list(second_value - first_value)
+                else:
+                    details["diff_groups"] = list(first_value - second_value)
+        elif self.event_data.request_code != OperationEvent.BIND:
+            details["target_dn"] = self.event_data.request["entry"]
+
+    def _prepare_details(self) -> dict:
+        """Prepare base details structure from event data."""
+        details = self.event_data.context.get("details", {})
+
+        if "LDAP" in self.event_data.protocol:
+            self._enrich_ldap_details(details)
+
+        return details
+
+    def _extract_error_info(self) -> dict[str, str]:
+        """Extract error information from failed event."""
+        if "error_code" in self.event_data.context.get("details", {}):
+            details = self.event_data.context["details"]
+            return {
+                "error_code": details["error_code"],
+                "error_message": details["error_message"],
+            }
+        elif self.event_data.protocol.endswith("LDAP"):
+            last_response = self.event_data.responses[-1]
+            return {
+                "error_code": last_response["result_code"],
+                "error_message": last_response["message"],
+            }
+        return {}
+
+    def build(self) -> NormalizedAuditEvent:
+        """Normalize event data и вернуть pydantic-модель."""
+        details = self._prepare_details()
+        if not self.trigger.is_operation_success:
+            details.update(self._extract_error_info())
+
+        return NormalizedAuditEvent(
+            **self._get_common_fields(),
+            details=details,
+        )
