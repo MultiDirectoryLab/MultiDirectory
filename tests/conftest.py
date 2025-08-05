811b443f53fe71f6ef2adb83211279cc68717b9d
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-08-05 10:37:54 +0300

    test: fix runtime error

--- tests/conftest.py
+++ tests/conftest.py
@@ -428,7 +428,6 @@ class TestProvider(Provider):
         )
         yield AuditRawManager(manager)
 
-        await client.flushdb()
         with suppress(RuntimeError):
             await client.aclose()
 
