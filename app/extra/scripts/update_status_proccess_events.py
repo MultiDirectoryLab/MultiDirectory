608b3150392049725b29192fa3fb3450e4902f38
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-08-08 15:49:26 +0300

    feat: implement update_status_process_events method in AuditUseCase for event processing management

--- app/extra/scripts/update_status_proccess_events.py
+++ app/extra/scripts/update_status_proccess_events.py
@@ -9,10 +9,4 @@ from ldap_protocol.policies.audit.audit_use_case import AuditUseCase
 
 async def update_status_process_events(audit_use_case: AuditUseCase) -> None:
     """Update the status of process events."""
-    if (
-        await audit_use_case.is_existing_active_policy()
-        and await audit_use_case.is_existing_active_destination()
-    ):
-        await audit_use_case.enable_event_processing()
-    else:
-        await audit_use_case.disable_event_processing()
+    await audit_use_case.update_status_process_events()
