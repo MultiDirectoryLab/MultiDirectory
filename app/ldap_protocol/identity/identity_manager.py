b9c78c2f802e79c6715d1c9c9ff8fc4a22f62513
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-08-08 12:25:06 +0300

    refactor: update AuditMonitor to use public attributes for IP and User-Agent

--- app/ldap_protocol/identity/identity_manager.py
+++ app/ldap_protocol/identity/identity_manager.py
@@ -106,8 +106,8 @@ class IdentityManager:
             ) -> object:
                 self._monitor.event_type = OperationEvent.BIND
                 self._monitor.username = form.username
-                self._monitor._ip = ip  # noqa
-                self._monitor._user_agent = user_agent  # noqa
+                self._monitor.ip = ip
+                self._monitor.user_agent = user_agent
                 try:
                     return await attr(
                         form=form,
