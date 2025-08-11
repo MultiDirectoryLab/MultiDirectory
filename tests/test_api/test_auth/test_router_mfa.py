726958d7a9565f1f2a7d368a253d4a44201c23a6
TheMihMih <thebestmihmihever@gmail.com>
2025-08-11 13:32:30 +0300

    tests: add /me check with pending mfa

--- tests/test_api/test_auth/test_router_mfa.py
+++ tests/test_api/test_auth/test_router_mfa.py
@@ -7,7 +7,9 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 from datetime import datetime, timedelta
 
 import httpx
+from loguru import logger
 import pytest
+from fastapi import status
 from jose import jwt
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -62,7 +64,7 @@ async def test_set_and_remove_mfa(
 @pytest.mark.asyncio
 @pytest.mark.usefixtures("setup_session")
 async def test_connect_mfa(
-    http_client: httpx.AsyncClient,
+    unbound_http_client: httpx.AsyncClient,
     session: AsyncSession,
     creds: TestCreds,
 ) -> None:
@@ -75,7 +77,7 @@ async def test_connect_mfa(
 
     redirect_url = "example.com"
 
-    response = await http_client.post(
+    response = await unbound_http_client.post(
         "/multifactor/connect",
         data={"username": creds.un, "password": creds.pw},
     )
@@ -84,6 +86,8 @@ async def test_connect_mfa(
         "status": MFAChallengeStatuses.PENDING,
         "message": redirect_url,
     }
+    response = await unbound_http_client.get("auth/me")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     user = await authenticate_user(session, creds.un, creds.pw)
 
@@ -93,7 +97,7 @@ async def test_connect_mfa(
 
     token = jwt.encode({"aud": "123", "uid": user.id, "exp": exp}, "123")
 
-    response = await http_client.post(
+    response = await unbound_http_client.post(
         "/multifactor/create",
         data={"accessToken": token},
         follow_redirects=False,
@@ -101,3 +105,6 @@ async def test_connect_mfa(
 
     assert response.status_code == 302
     assert response.cookies.get("id")
+
+    resp = await unbound_http_client.get("auth/me")
+    assert resp.status_code == status.HTTP_200_OK
