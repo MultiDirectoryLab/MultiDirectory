e7696933ea263bed2ad51fb94a10c895fb05d45f
Naksen <khannanov.rus@gmail.com>
2025-08-13 20:03:29 +0300

    refactor: improve error handling and remove unused classes in RoleDAO

--- app/ldap_protocol/roles/role_dao.py
+++ app/ldap_protocol/roles/role_dao.py
@@ -4,17 +4,15 @@ Copyright (c) 2025 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
-from dataclasses import dataclass
-
 from adaptix import P
 from adaptix.conversion import get_converter, link_function
 from sqlalchemy import and_, delete, func, select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload, selectinload
 
 from abstract_dao import AbstractDAO
-from enums import AceType, RoleScope
-from ldap_protocol.utils.const import GRANT_DN_STRING
+from enums import RoleScope
 from ldap_protocol.utils.helpers import get_depth_by_dn
 from ldap_protocol.utils.queries import (
     get_groups,
@@ -23,33 +21,14 @@ from ldap_protocol.utils.queries import (
 )
 from models import AccessControlEntry, Directory, Group, Role
 
-
-@dataclass
-class RoleDTO:
-    """Data Transfer Object for Role."""
-
-    name: str
-    creator_upn: str | None
-    is_system: bool
-    groups: list[str]
-    id: int | None = None
-
-    def get_id(self) -> int:
-        if self.id is None:
-            raise ValueError("Non read model value")
-        return self.id
-
-
-@dataclass
-class AccessControlEntrySchema:
-    """Base schema Access Control Entry."""
-
-    ace_type: AceType
-    scope: RoleScope
-    base_dn: GRANT_DN_STRING
-    attribute_type_id: int | None = None
-    entity_type_id: int | None = None
-    is_allow: bool = True
+from .dataclasses import AccessControlEntryDTO, RoleDTO
+from .exceptions import (
+    AccessControlEntryAddError,
+    AccessControlEntryNotFoundError,
+    NoValidDistinguishedNameError,
+    NoValidGroupsError,
+    RoleNotFoundError,
+)
 
 
 def make_groups(role: Role) -> list[str]:
@@ -85,7 +64,7 @@ class RoleDAO(AbstractDAO[RoleDTO]):
         query = (
             select(Role)
             .options(
-                selectinload(Role.groups),
+                selectinload(Role.groups).selectinload(Group.directory),
                 selectinload(Role.access_control_entries).options(
                     joinedload(AccessControlEntry.attribute_type),
                     joinedload(AccessControlEntry.entity_type),
@@ -96,7 +75,7 @@ class RoleDAO(AbstractDAO[RoleDTO]):
         )
         retval = await self._session.scalar(query)
         if not retval:
-            raise ValueError(f"Role with ID {_id} does not exist.")
+            raise RoleNotFoundError(f"Role with ID {_id} does not exist.")
         return retval
 
     async def get(self, _id: int) -> RoleDTO:
@@ -107,7 +86,7 @@ class RoleDAO(AbstractDAO[RoleDTO]):
         """
         return _convert(await self._get_raw(_id))
 
-    async def get_by_name(self, role_name: str) -> Role | None:
+    async def get_by_name(self, role_name: str) -> RoleDTO:
         """Get a role by its name.
 
         :param role_name: Name of the role to retrieve.
@@ -116,7 +95,7 @@ class RoleDAO(AbstractDAO[RoleDTO]):
         query = (
             select(Role)
             .options(
-                selectinload(Role.groups),
+                selectinload(Role.groups).selectinload(Group.directory),
                 selectinload(Role.access_control_entries).options(
                     joinedload(AccessControlEntry.attribute_type),
                     joinedload(AccessControlEntry.entity_type),
@@ -125,8 +104,12 @@ class RoleDAO(AbstractDAO[RoleDTO]):
             )
             .where(Role.name == role_name)
         )
-        result = await self._session.execute(query)
-        return result.scalars().first()
+        retval = await self._session.scalar(query)
+        if not retval:
+            raise RoleNotFoundError(
+                f"Role with name {role_name} does not exist.",
+            )
+        return _convert(retval)
 
     async def get_all(self) -> list[RoleDTO]:
         """Get all roles.
@@ -135,7 +118,10 @@ class RoleDAO(AbstractDAO[RoleDTO]):
         """
         roles = (
             await self._session.scalars(
-                select(Role).options(selectinload(Role.groups)),
+                select(Role)
+                .options(
+                    selectinload(Role.groups).selectinload(Group.directory),
+                ),
             )
         ).all()
         return list(map(_convert, roles))
@@ -157,7 +143,7 @@ class RoleDAO(AbstractDAO[RoleDTO]):
             session=self._session,
         )
         if not groups:
-            raise ValueError("No valid groups provided for the role.")
+            raise NoValidGroupsError("No valid groups provided for the role.")
 
         role = Role(
             name=dto.name,
@@ -201,7 +187,7 @@ class RoleDAO(AbstractDAO[RoleDTO]):
         )
 
         if not groups:
-            raise ValueError("No valid groups provided for the role.")
+            raise NoValidGroupsError("No valid groups provided for the role.")
 
         role.name = dto.name
         role.creator_upn = dto.creator_upn
@@ -272,13 +258,12 @@ class RoleDAO(AbstractDAO[RoleDTO]):
     async def add_access_control_entries(
         self,
         role_id: int,
-        access_control_entries: list[AccessControlEntrySchema],
-    ) -> list[AccessControlEntry]:
+        access_control_entries: list[AccessControlEntryDTO],
+    ) -> None:
         """Add new access control entries to a role.
 
         :param role_id: ID of the role to add entries to.
         :param access_control_entries: List of access control entries to add.
-        :return: List of newly added AccessControlEntry objects.
         """
         role = await self._get_raw(role_id)
 
@@ -292,6 +277,11 @@ class RoleDAO(AbstractDAO[RoleDTO]):
                 ] = await self._get_directories_with_scope(
                     base_dn=ace.base_dn,
                     scope=ace.scope,
+                )
+
+            if not directory_cache[cache_key]:
+                raise NoValidDistinguishedNameError(
+                    f"Invalid distinguished name: {ace.base_dn}",
                 )
 
             new_ace = AccessControlEntry(
@@ -308,15 +298,19 @@ class RoleDAO(AbstractDAO[RoleDTO]):
             new_aces.append(new_ace)
 
         self._session.add_all(new_aces)
-        await self._session.flush()
+        try:
+            await self._session.flush()
+        except IntegrityError:
+            await self._session.rollback()
+            raise AccessControlEntryAddError(
+                "Failed to add access control entries.",
+            )
 
         for single_ace in new_aces:
             await self._session.refresh(
                 single_ace,
                 attribute_names=["attribute_type", "entity_type"],
             )
-
-        return new_aces
 
     async def delete_access_control_entry(self, ace_id: int) -> None:
         """Delete an access control entry from a role.
@@ -325,29 +319,9 @@ class RoleDAO(AbstractDAO[RoleDTO]):
         """
         ace = await self._session.get(AccessControlEntry, ace_id)
         if not ace:
-            raise ValueError(f"ACE with ID {ace_id} does not exist.")
+            raise AccessControlEntryNotFoundError(
+                f"ACE with ID {ace_id} does not exist.",
+            )
 
         await self._session.delete(ace)
         await self._session.flush()
-
-    async def get_aces_by_dn(
-        self,
-        base_dn: GRANT_DN_STRING,
-    ) -> list[AccessControlEntry]:
-        """Get access control entries by base DN.
-
-        :param base_dn: Base DN to filter access control entries.
-        :return: List of AccessControlEntry objects.
-        """
-        query = (
-            select(AccessControlEntry)
-            .join(AccessControlEntry.directories)
-            .where(Directory.path == get_search_path(base_dn))
-            .options(
-                joinedload(AccessControlEntry.role),
-                joinedload(AccessControlEntry.attribute_type),
-                joinedload(AccessControlEntry.entity_type),
-            )
-        )
-        result = await self._session.scalars(query)
-        return list(result.all())
