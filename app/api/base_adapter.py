55edb800c6eb77cdeb099ad86d900492ef2d0423
Mastermind-U <rex49513@gmail.com>
2025-08-18 14:27:49 +0300

    feat: add BaseAdapter class for async DAO wrapping

---
+++ app/api/base_adapter.py
@@ -0,0 +1,56 @@
+"""Base Adapter.
+
+Copyright (c) 2024 MultiFactor
+License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
+"""
+
+from asyncio import iscoroutinefunction
+from functools import wraps
+from typing import Callable, ParamSpec, Protocol, TypeVar
+
+from fastapi import HTTPException
+
+_P = ParamSpec("_P")
+_R = TypeVar("_R")
+_T = TypeVar("_T")
+
+
+class BaseAdapter(Protocol[_T]):
+    """Abstract Adapter interface."""
+
+    _exceptions_map: dict[type[Exception], int]
+    _service: _T
+
+    def __getattribute__(self, name: str) -> object:
+        """Override attribute access to wrap DAO in an async wrapper."""
+        attr = super().__getattribute__(name)
+
+        if not callable(attr):
+            return attr
+
+        def safecall(func: Callable[_P, _R]) -> Callable[_P, _R]:
+            @wraps(func)
+            async def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _R:
+                try:
+                    if iscoroutinefunction(func):
+                        retval = await func(*args, **kwargs)
+                    else:
+                        retval = func(*args, **kwargs)
+
+                except Exception as err:
+                    code = self._exceptions_map.get(type(err))
+
+                    if code is None:
+                        raise
+
+                    raise HTTPException(
+                        status_code=code,
+                        detail=str(err),
+                    ) from err
+
+                else:
+                    return retval
+
+            return wrapper  # type: ignore
+
+        return safecall(attr)
