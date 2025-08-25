4c367ac17216c5b024a92e5715a4db2302458790
iyashnov <x13777x@gmail.com>
2025-08-25 14:03:57 +0300

    refactor: update subnet handling in KeaDHCPManager and improve exception usage

--- app/ldap_protocol/dhcp/exceptions.py
+++ app/ldap_protocol/dhcp/exceptions.py
@@ -6,12 +6,24 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE.
 
 
 class DHCPError(Exception):
-    """DHCP manager error."""
+    """DHCP base exception."""
 
 
-class DHCPAPIError(Exception):
+class DHCPAPIError(DHCPError):
     """DHCP API error."""
 
 
 class DHCPConnectionError(ConnectionError):
     """DHCP connection error."""
+
+
+class DHCPEntryAddError(DHCPError):
+    """DHCP entry addition error."""
+
+
+class DHCPEntryNotFoundError(DHCPError):
+    """DHCP entry not found error."""
+
+
+class DHCPEntryUpdateError(DHCPError):
+    """DHCP entry update error."""
