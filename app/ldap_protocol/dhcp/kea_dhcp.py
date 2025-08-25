4bc649c2b84f88f984e4023ed607a91131969794
iyashnov <x13777x@gmail.com>
2025-08-25 12:15:09 +0300

    refactor: move DHCPManagerState and KeaDHCPCommands to enums.py for better organization

--- app/ldap_protocol/dhcp/kea_dhcp.py
+++ app/ldap_protocol/dhcp/kea_dhcp.py
@@ -4,30 +4,13 @@ Copyright (c) 2025 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
-from enum import StrEnum
 from ipaddress import IPv4Address, IPv4Network
 
 from pydantic import BaseModel
 
 from .base import AbstractDHCPManager
+from .enums import KeaDHCPCommands
 from .exceptions import DHCPError
-
-
-class KeaDHCPCommands(StrEnum):
-    """Kea DHCP API commands."""
-
-    NETWORK4_ADD = "network4-add"
-    NETWORK4_DEL = "network4-del"
-    NETWORK4_LIST = "network4-list"
-    NETWORK4_GET = "network4-get"
-    LEASE4_ADD = "lease4-add"
-    LEASE4_DEL = "lease4-del"
-    LEASE4_LIST = "lease4-list"
-    LEASE4_GET_BY_HW_ADDRESS = "lease4-get-by-hw-address"
-    LEASE4_GET_BY_HOSTNAME = "lease4-get-by-hostname"
-    RESERVATION_ADD = "reservation-add"
-    RESERVATION_DEL = "reservation-del"
-    RESERVATION_LIST = "reservation-list"
 
 
 class KeaDHCPAPIRequest(BaseModel):
