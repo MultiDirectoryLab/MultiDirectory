b830593c5436f5d5db21135efe5980f654a42241
Mastermind-U <rex49513@gmail.com>
2025-08-29 19:02:13 +0300

    refactor: enhance load_params handling in build_paginated_search_query

--- app/ldap_protocol/utils/pagination.py
+++ app/ldap_protocol/utils/pagination.py
@@ -7,12 +7,13 @@ License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE.
 import sys
 from dataclasses import dataclass
 from math import ceil
-from typing import Sequence, TypeVar
+from typing import Iterable, Sequence, TypeVar
 
 from pydantic import BaseModel, Field
 from sqlalchemy import Column, func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import InstrumentedAttribute
+from sqlalchemy.orm.strategy_options import _AbstractLoad
 from sqlalchemy.sql.expression import Select
 
 P = TypeVar("P", contravariant=True, bound=BaseModel)
@@ -40,9 +41,15 @@ def build_paginated_search_query[S](
     order_by_field: InstrumentedAttribute | Column,
     params: PaginationParams,
     search_field: InstrumentedAttribute | Column | None = None,
+    load_params: Iterable[_AbstractLoad] | _AbstractLoad | None = None,
 ) -> Select[tuple[S]]:
     """Build query."""
     query = select(model).order_by(order_by_field)
+
+    if load_params is not None:
+        if not isinstance(load_params, Iterable):
+            load_params = [load_params]
+        query = query.options(*load_params)
 
     if params.query:
         if search_field is None:
