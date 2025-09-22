4356dce23dee20e07441f736c8d6c5d0a7422db9
Nikita Ulyanov <69312634+rimu-stack@users.noreply.github.com>
2025-09-22 17:06:30 +0300

    add: migration from enterprise (#733)

---
+++ app/alembic/versions/eeaed5989eb0_group_policies.py
@@ -0,0 +1,21 @@
+"""Initial Group Policies.
+
+Revision ID: eeaed5989eb0
+Revises: e4d6d99d32bd
+Create Date: 2025-08-01 08:58:41.760249
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "eeaed5989eb0"
+down_revision = "e4d6d99d32bd"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Upgrade."""
+
+
+def downgrade() -> None:
+    """Downgrade."""
