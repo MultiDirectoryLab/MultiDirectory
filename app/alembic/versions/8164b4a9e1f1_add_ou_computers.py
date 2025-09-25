5c8696d995d67a9df4c16f5f95c71de0f152217c
Milov Dmitriy <milovdd@mail.ru>
2025-09-25 10:45:20 +0300

    fix: restructure task_760

--- app/alembic/versions/8164b4a9e1f1_add_ou_computers.py
+++ app/alembic/versions/8164b4a9e1f1_add_ou_computers.py
@@ -8,8 +8,7 @@ Create Date: 2025-09-24 09:37:33.334259
 
 from alembic import op
 from sqlalchemy import delete, exists, select
-from sqlalchemy.exc import DBAPIError, IntegrityError
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncConnection, AsyncSession
 from sqlalchemy.orm import selectinload
 
 from extra.setup_dev import create_dir
@@ -36,65 +35,60 @@ _OU_COMPUTERS_DATA = {
 def upgrade() -> None:
     """Upgrade."""
 
-    async def _create_ou_computers(connection: AsyncSession) -> None:
+    async def _create_ou_computers(connection: AsyncConnection) -> None:
         session = AsyncSession(bind=connection)
         await session.begin()
 
         if not await get_base_directories(session):
             return
 
-        ou_computers = await session.scalar(
+        exists_ou_computers = await session.scalar(
             select(
                 exists(Directory)
                 .where(Directory.name == "computers"),
             ),
         )  # fmt: skip
-        if ou_computers:
+        if exists_ou_computers:
             return
 
-        try:
-            domain_dir = await session.scalar(
-                select(Directory)
-                .where(Directory.parent_id.is_(None)),
-            )  # fmt: skip
+        domain_dir = await session.scalar(
+            select(Directory)
+            .where(Directory.parent_id.is_(None)),
+        )  # fmt: skip
 
-            await create_dir(
-                _OU_COMPUTERS_DATA,
-                session,
-                domain_dir,
-                PasswordValidator(),
-                domain_dir,
+        await create_dir(
+            _OU_COMPUTERS_DATA,
+            session,
+            domain_dir,
+            PasswordValidator(),
+            domain_dir,
+        )
+
+        ou_computers_dir_id = await session.scalar(
+            select(Directory.id)
+            .where(Directory.name == "computers"),
+        )  # fmt: skip
+
+        role_names = (
+            RoleConstants.DOMAIN_ADMINS_ROLE_NAME,
+            RoleConstants.READ_ONLY_ROLE_NAME,
+        )
+        roles_res = await session.scalars(
+            select(Role)
+            .where(Role.name.in_((role_names)))
+            .options(selectinload(Role.access_control_entries)),
+        )
+        roles = roles_res.all()
+
+        members = [
+            AccessControlEntryDirectoryMembership(
+                access_control_entry_id=ace.id,
+                directory_id=ou_computers_dir_id,
             )
-
-            ou_computers_dir_id = await session.scalar(
-                select(Directory.id)
-                .where(Directory.name == "computers"),
-            )  # fmt: skip
-
-            role_names = (
-                RoleConstants.DOMAIN_ADMINS_ROLE_NAME,
-                RoleConstants.READ_ONLY_ROLE_NAME,
-            )
-            roles_res = await session.scalars(
-                select(Role)
-                .where(Role.name.in_((role_names)))
-                .options(selectinload(Role.access_control_entries)),
-            )
-            roles = roles_res.all()
-
-            members = [
-                AccessControlEntryDirectoryMembership(
-                    access_control_entry_id=ace.id,
-                    directory_id=ou_computers_dir_id,
-                )
-                for role in roles
-                for ace in role.access_control_entries
-            ]
-            session.add_all(members)
-
-            await session.flush()
-        except (IntegrityError, DBAPIError):
-            pass
+            for role in roles
+            for ace in role.access_control_entries
+        ]
+        session.add_all(members)
 
         await session.commit()
         await session.close()
