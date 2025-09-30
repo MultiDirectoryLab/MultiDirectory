91e96c22924fb5b4c7941d23ab8771a39e003d8f
Naksen <khannanov.rus@gmail.com>
2025-09-30 11:46:42 +0300

    add: DOMAIN_COMPUTERS_GROUP_NAME constant

--- app/ldap_protocol/utils/const.py
+++ app/ldap_protocol/utils/const.py
@@ -32,3 +32,4 @@ def _type_validate_email(email: str) -> str:
 GRANT_DN_STRING = Annotated[str, AfterValidator(_type_validate_entry)]
 EmailStr = Annotated[str, AfterValidator(_type_validate_email)]
 DOMAIN_USERS_GROUP_NAME = "domain users"
+DOMAIN_COMPUTERS_GROUP_NAME = "domain computers"
