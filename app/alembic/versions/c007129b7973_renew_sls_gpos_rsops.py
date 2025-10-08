892c39789ac994629567b33fa0f5497339bb667a
Михаил Михайлов <90135860+TheMihMih@users.noreply.github.com>
2025-10-08 13:04:44 +0300

    add: migration empty file (#755)

---
+++ app/alembic/versions/c007129b7973_renew_sls_gpos_rsops.py
@@ -0,0 +1,21 @@
+"""Delete rsops and incorrect gpos.
+
+Revision ID: c007129b7973
+Revises: 8164b4a9e1f1
+Create Date: 2025-10-03 09:35:10.399265
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "c007129b7973"
+down_revision = "8164b4a9e1f1"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Upgrade."""
+
+
+def downgrade() -> None:
+    """Downgrade."""
