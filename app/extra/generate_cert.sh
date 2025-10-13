6f0ebf80768d09089622d39d8bf94a9afea62d4f
TheMihMih <thebestmihmihever@gmail.com>
2025-10-13 17:47:05 +0300

    add: kadmin var to generate sh

--- app/extra/generate_cert.sh
+++ app/extra/generate_cert.sh
@@ -3,14 +3,17 @@
 CERT_PATH="/certs/krbcert.pem"
 EXPIRATION_THRESHOLD=30
 LIFESPAN_IN_DAYS=1095   # 3 years
+KRB5_CONFIG_SERVER=${KRB5_CONFIG_SERVER:-kadmin_api}
+
+echo $KRB5_CONFIG_SERVER
 
 generate_certificate() {
     openssl req -nodes -new -x509 \
         -days $LIFESPAN_IN_DAYS \
         -keyout /certs/krbkey.pem \
         -out /certs/krbcert.pem \
-        -addext "subjectAltName=DNS:kadmin_api" \
-        -subj '/C=RU/ST=Moscow/L=Moscow/O=Global Security/OU=Multifactor/CN=kadmin_api' > /dev/null 2>&1
+        -addext "subjectAltName=DNS:$KRB5_CONFIG_SERVER" \
+        -subj "/C=RU/ST=Moscow/L=Moscow/O=Global Security/OU=Multifactor/CN=$KRB5_CONFIG_SERVER" > /dev/null 2>&1
 }
 
 if [[ ! -f "$CERT_PATH" ]]; then
