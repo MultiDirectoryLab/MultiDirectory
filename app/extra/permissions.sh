1072a9a1bad9243529fab87aed0cc79ea5a935f5
TheMihMih <thebestmihmihever@gmail.com>
2025-10-17 13:26:45 +0300

    fix: integrations

--- app/extra/permissions.sh
+++ app/extra/permissions.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 chown -R md:md /app \
+            /app/logs/* \
             /venvs \
             /LDAP_keytab \
             /certs \
             /DNS_server_file \
             /DNS_server_configs  \
-            /audit \
+            /resolv.conf  \
+            /audit  || true
