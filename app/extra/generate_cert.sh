61ed5398518cc89a93d2cfc02331dbde7a58dd00
TheMihMih <thebestmihmihever@gmail.com>
2025-10-20 12:07:45 +0300

    refactor: add CERT_PATH KEY_PATH vars to gen

--- app/extra/generate_cert.sh
+++ app/extra/generate_cert.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-CERT_PATH="/certs/krbcert.pem"
+KEY_PATH=${KADMIN_KEY:-"/certs/krbkey.pem"}
+CERT_PATH=${KADMIN_CERT:-"/certs/krbcert.pem"}
 EXPIRATION_THRESHOLD=30
 LIFESPAN_IN_DAYS=1095   # 3 years
 KADMIN_API_SERVER=${KADMIN_API_SERVER:-kadmin_api}
@@ -10,8 +11,8 @@ echo $KRB5_CONFIG_SERVER
 generate_certificate() {
     openssl req -nodes -new -x509 \
         -days $LIFESPAN_IN_DAYS \
-        -keyout /certs/krbkey.pem \
-        -out /certs/krbcert.pem \
+        -keyout $KEY_PATH \
+        -out $CERT_PATH \
         -addext "subjectAltName=DNS:$KADMIN_API_SERVER" \
         -subj "/C=RU/ST=Moscow/L=Moscow/O=Global Security/OU=Multifactor/CN=$KADMIN_API_SERVER" > /dev/null 2>&1
 }
