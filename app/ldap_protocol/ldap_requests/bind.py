546b530bba8b0b2a7d38c66fa031f1895c115ef7
Naksen <khannanov.rus@gmail.com>
2025-10-24 19:01:05 +0300

    refactor: password change requirement check in BindRequest

--- app/ldap_protocol/ldap_requests/bind.py
+++ app/ldap_protocol/ldap_requests/bind.py
@@ -36,14 +36,12 @@ from ldap_protocol.policies.network_policy import (
     check_mfa_group,
     is_user_group_valid,
 )
+from ldap_protocol.policies.password.use_cases import PasswordPolicyUseCases
 from ldap_protocol.user_account_control import (
     UserAccountControlFlag,
     get_check_uac,
 )
-from ldap_protocol.utils.queries import (
-    check_kerberos_group,
-    set_user_logon_attrs,
-)
+from ldap_protocol.utils.queries import set_user_logon_attrs
 
 from .base import BaseRequest
 from .contexts import LDAPBindRequestContext, LDAPUnbindRequestContext
@@ -132,6 +130,30 @@ class BindRequest(BaseRequest):
         except MultifactorAPI.MultifactorError:
             return bool(policy.bypass_service_failure)
 
+    @staticmethod
+    async def is_required_password_change(
+        user: User,
+        password_use_case: PasswordPolicyUseCases,
+    ) -> bool:
+        """Check if user required to change password.
+
+        :param User user: user
+        :param PasswordPolicyUseCases password_use_case: password use case
+        :param AsyncSession session: db session
+        :return bool: required or not
+        """
+        pwd_last_set = await password_use_case.get_or_create_pwd_last_set(
+            user.directory_id,
+        )
+        password_policy = await password_use_case.get_password_policy()
+        is_pwd_expired = await password_use_case.check_expired_max_age(
+            password_policy,
+            user,
+            pwd_last_set,
+        )
+
+        return pwd_last_set == "0" or is_pwd_expired  # noqa: S105
+
     async def handle(
         self,
         ctx: LDAPBindRequestContext,
@@ -186,25 +208,16 @@ class BindRequest(BaseRequest):
             yield get_bad_response(LDAPBindErrors.LOGON_FAILURE)
             return
 
-        pwd_last_set = await ctx.password_use_cases.get_or_create_pwd_last_set(
-            user.directory_id,
-        )
-        is_pwd_expired = await ctx.password_use_cases.check_expired_max_age(
-            user,
-            pwd_last_set,
-        )
-
-        is_krb_user = await check_kerberos_group(user, ctx.session)
-
-        required_pwd_change = (
-            pwd_last_set == "0" or is_pwd_expired  # noqa: S105
-        ) and not is_krb_user
-
         if user.is_expired():
             yield get_bad_response(LDAPBindErrors.ACCOUNT_EXPIRED)
             return
 
-        if required_pwd_change:
+        if not uac_check(UserAccountControlFlag.DONT_EXPIRE_PASSWORD) and (
+            await self.is_required_password_change(
+                user,
+                ctx.password_use_cases,
+            )
+        ):
             yield get_bad_response(LDAPBindErrors.PASSWORD_MUST_CHANGE)
             return
 
