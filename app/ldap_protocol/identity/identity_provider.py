0e0ecdc8c69468cfa8804520524761ec0a9b4b62
TheMihMih <thebestmihmihever@gmail.com>
2025-11-05 14:58:10 +0300

    refactor: ruff

--- app/ldap_protocol/identity/identity_provider.py
+++ app/ldap_protocol/identity/identity_provider.py
@@ -4,7 +4,6 @@ Copyright (c) 2025 MultiFactor
 License: https://github.com/MultiDirectoryLab/MultiDirectory/blob/main/LICENSE
 """
 
-
 from abstract_dao import AbstractService
 from config import Settings
 from entities import User
