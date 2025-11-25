ee0bc49eefab06dc027a2baa8576f116471bf84b
m.shvets <m89258262994@yandex.com>
2025-11-25 11:20:12 +0300

    Add pre_run_manager service to multiple docker-compose files for setup execution and dependency management.

--- app/extra/pre_run_setup.sh
+++ app/extra/pre_run_setup.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-chown -R md:md /app \
-            /app/logs \
-            /venvs \
-            /LDAP_keytab \
-            /certs \
-            /DNS_server_file \
-            /DNS_server_configs  \
-            /audit  || true
-chown md:md /resolv.conf || true
 
 sed -i 's/ou=users/cn=users/g' /etc/kdc/krb5.d/stash.keyfile || true
 sed -i 's/ou=users/cn=users/g' /etc/kdc/krb5.conf || true
