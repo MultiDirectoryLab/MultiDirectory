f4e1892042d2199a2825c5e06d17de54e8567efa
m.shvets <m89258262994@yandex.com>
2025-11-26 05:11:01 +0300

    Add DNS server file and configuration volumes to docker-compose files; update ownership in pre_run_setup.sh for proper permissions.

--- app/extra/pre_run_setup.sh
+++ app/extra/pre_run_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-
+chown - R bind:bind /opt/dns_server_file/
+chown - R bind:bind /etc/bind/dns_server_config/
 sed -i 's/ou=users/cn=users/g' /etc/kdc/krb5.d/stash.keyfile || true
 sed -i 's/ou=users/cn=users/g' /etc/kdc/krb5.conf || true
