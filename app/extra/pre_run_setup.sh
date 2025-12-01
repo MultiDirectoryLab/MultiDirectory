1e37f05c769bcabb40b08abe13db89187473197e
m.shvets <m89258262994@yandex.com>
2025-12-01 17:50:50 +0300

    Update ownership commands in pre_run_setup.sh to set permissions for rndc.key

--- app/extra/pre_run_setup.sh
+++ app/extra/pre_run_setup.sh
@@ -8,7 +8,8 @@ RUN useradd -m -u $UID -g $GID -o -s /bin/bash $UNAME
 USER $UNAME
 CMD /bin/bash
 
-chown - R $UID:$GID /opt/dns_server_file/
-chown - R $UID:$GID /etc/bind/dns_server_config/
+chown $UID:$GID /etc/bind/rndc.key
+# chown - R $UID:$GID /opt/dns_server_file/
+# chown - R $UID:$GID /etc/bind/dns_server_config/
 sed -i 's/ou=users/cn=users/g' /etc/kdc/krb5.d/stash.keyfile || true
 sed -i 's/ou=users/cn=users/g' /etc/kdc/krb5.conf || true
