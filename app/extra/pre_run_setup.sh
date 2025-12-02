62b1155e944484d0ff327a6f3f99221fbc193d37
m.shvets <m89258262994@yandex.com>
2025-12-02 11:42:07 +0300

    Remove pre_run_manager service and associated DNS server volume paths from docker-compose files; clean up pre_run_setup.sh by eliminating unused commands.

--- app/extra/pre_run_setup.sh
+++ app/extra/pre_run_setup.sh
@@ -2,10 +2,3 @@
 
 sed -i 's/ou=users/cn=users/g' /etc/kdc/krb5.d/stash.keyfile || true
 sed -i 's/ou=users/cn=users/g' /etc/kdc/krb5.conf || true
-
-RNDC_KEY="/etc/bind/rndc.key"
-
-# if [ -f "$RNDC_KEY" ]; then
-#     chown 101:101 "$RNDC_KEY" || true
-#     chmod 640 "$RNDC_KEY" || true
-# fi
