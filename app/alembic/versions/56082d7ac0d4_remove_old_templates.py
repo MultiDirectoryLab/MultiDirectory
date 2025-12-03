b61b2daad2f3026cfc5de8b86a6938f24349a116
Nikita Ulyanov <var.ulyanov@gmail.com>
2025-12-03 14:42:07 +0300

    add: migration to remove old GPO instruction templates

---
+++ app/alembic/versions/56082d7ac0d4_remove_old_templates.py
@@ -0,0 +1,21 @@
+"""Remove old instructions templates.
+
+Revision ID: 56082d7ac0d4
+Revises: 16a9fa2c1f1e
+Create Date: 2025-12-03 11:07:16.097669
+
+"""
+
+# revision identifiers, used by Alembic.
+revision: None | str = "56082d7ac0d4"
+down_revision: None | str = "16a9fa2c1f1e"
+branch_labels: None | list[str] = None
+depends_on: None | list[str] = None
+
+
+def upgrade() -> None:
+    """Upgrade."""
+
+
+def downgrade() -> None:
+    """Downgrade."""
