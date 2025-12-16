33a7d40ea9a14667198b67e75e21ddf2671080d9
Naksen <khannanov.rus@gmail.com>
2025-12-16 13:22:20 +0300

    refactor: add blank line for improved readability in LDAP context provider and request context files

--- app/ioc.py
+++ app/ioc.py
@@ -535,6 +535,7 @@ class LDAPContextProvider(Provider):
             rootdse_rd=RootDSEReader(settings, SADomainGateway(session)),
         )
 
+
 class HTTPProvider(LDAPContextProvider):
     """HTTP LDAP session."""
 
