fb832cbf1dd605e3658f0d5a8ff33ba340af69c9
m.shvets <m89258262994@yandex.com>
2026-01-13 16:32:22 +0300

    Fix: update hash assertion in kadmin setup test

--- tests/test_api/test_main/test_kadmin.py
+++ tests/test_api/test_main/test_kadmin.py
@@ -157,7 +157,7 @@ async def test_setup_call(
     kdc_doc = kadmin.setup.call_args.kwargs.pop("kdc_config").encode()
 
     # NOTE: Asserting documents integrity, tests template rendering
-    assert blake2b(krb_doc, digest_size=8).hexdigest() == "f433bbc7df5a236b"
+    assert blake2b(krb_doc, digest_size=8).hexdigest() == "0567ec28b8ccca51"
     assert blake2b(kdc_doc, digest_size=8).hexdigest() == "79e43649d34fe577"
 
     assert kadmin.setup.call_args.kwargs == {
