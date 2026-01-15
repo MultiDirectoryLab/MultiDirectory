2858d4be13c0c9ca81a8cf47914a1acdd0288f23
Misha-Shvets <76677350+Misha-Shvets@users.noreply.github.com>
2026-01-15 12:10:35 +0300

    Add KRBAPIConnectionError handling to error map in krb5_router.py (#885)

--- app/api/main/krb5_router.py
+++ app/api/main/krb5_router.py
@@ -33,6 +33,7 @@ from ldap_protocol.kerberos.exceptions import (
     KerberosDependencyError,
     KerberosNotFoundError,
     KerberosUnavailableError,
+    KRBAPIConnectionError,
 )
 from ldap_protocol.ldap_requests.contexts import LDAPAddRequestContext
 from ldap_protocol.utils.const import EmailStr
@@ -60,6 +61,10 @@ error_map: ERROR_MAP_TYPE = {
         translator=translator,
     ),
     KerberosUnavailableError: rule(
+        status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        translator=translator,
+    ),
+    KRBAPIConnectionError: rule(
         status=status.HTTP_500_INTERNAL_SERVER_ERROR,
         translator=translator,
     ),
