240de5d126453d8c04feecf9f74b637149a6d7bd
m.shvets <m89258262994@yandex.com>
2026-01-21 14:29:46 +0300

    Refactor: remove user creation assertions in test_ldap_modify_with_ap to simplify test logic

--- tests/test_ldap/test_util/test_modify.py
+++ tests/test_ldap/test_util/test_modify.py
@@ -17,7 +17,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload, selectinload, subqueryload
 
 from config import Settings
-from entities import Directory, Group, User
+from entities import Directory, Group
 from enums import AceType, RoleScope
 from ldap_protocol.kerberos.base import AbstractKadmin
 from ldap_protocol.ldap_codes import LDAPCodes
@@ -670,18 +670,6 @@ async def test_ldap_modify_with_ap(
 
     directory = await session.scalar(query)
 
-    if directory and not directory.user:
-        user = User(
-            sam_account_name="test",
-            user_principal_name="test@md.test",
-            mail="users@md.test",
-            display_name="test",
-            directory_id=directory.id,
-        )
-        session.add(user)
-        await session.commit()
-        await session.refresh(directory)
-
     async def try_modify() -> int:
         with tempfile.NamedTemporaryFile("w") as file:
             file.write(
@@ -789,8 +777,6 @@ async def test_ldap_modify_with_ap(
     ]
 
     assert attributes["jpegPhoto"] == ["modme.jpeg"]
-    assert directory.user
-    assert directory.user.mail == "modme@student.of.life.edu"
 
     assert "posixEmail" not in attributes
 
