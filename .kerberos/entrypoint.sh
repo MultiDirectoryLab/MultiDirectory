0d2f9d3c86fa54ef52a04ecf336037bf33426007
Nikita Ulyanov <var.ulyanov@gmail.com>
2026-01-22 12:30:28 +0300

    fix: replace services with system

--- .kerberos/entrypoint.sh
+++ .kerberos/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 sed -i 's/ou=users/cn=users/g' /etc/krb5.d/stash.keyfile || true
 sed -i 's/ou=users/cn=users/g' /etc/krb5.conf || true
-sed -i 's/ou=System/ou=services/g' /etc/krb5.conf || true
+sed -i 's/ou=services/ou=System/g' /etc/krb5.conf || true
 
 cd /server
 
