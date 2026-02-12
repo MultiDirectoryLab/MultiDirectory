25ed411d2e70b2d5b26d7bbb2c9899e51843fe19
Ruslan <khannanov.rus@gmail.com>
2026-02-12 14:37:16 +0300

    Fix ModifyDN access check (#932)

--- app/ldap_protocol/ldap_requests/delete.py
+++ app/ldap_protocol/ldap_requests/delete.py
@@ -75,6 +75,7 @@ class DeleteRequest(BaseRequest):
             select(Directory)
             .options(
                 joinedload(qa(Directory.user)),
+                joinedload(qa(Directory.entity_type)),
                 selectinload(qa(Directory.groups)).selectinload(
                     qa(Group.directory),
                 ),
