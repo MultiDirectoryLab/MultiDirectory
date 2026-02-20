afa1f8d45b746dc5a2fc104165e5d81361b6f78f
Naksen <khannanov.rus@gmail.com>
2026-02-20 15:37:49 +0300

    fix: update revision identifiers and improve container scope usage in krbadmin access fix

--- app/alembic/versions/19d86e660cf2_fix_krbadmin_access.py
+++ app/alembic/versions/19d86e660cf2_fix_krbadmin_access.py
@@ -1,13 +1,13 @@
 """Fix krbadmin access.
 
 Revision ID: 19d86e660cf2
-Revises: 2dadf40c026a
+Revises: ebf19750805e
 Create Date: 2026-02-19 11:40:15.805997
 
 """
 
 from alembic import op
-from dishka import AsyncContainer
+from dishka import AsyncContainer, Scope
 from sqlalchemy.ext.asyncio import AsyncConnection, AsyncSession
 
 from enums import RoleConstants
@@ -18,7 +18,7 @@ from ldap_protocol.utils.queries import get_base_directories
 
 # revision identifiers, used by Alembic.
 revision: None | str = "19d86e660cf2"
-down_revision: None | str = "2dadf40c026a"
+down_revision: None | str = "ebf19750805e"
 branch_labels: None | list[str] = None
 depends_on: None | list[str] = None
 
@@ -27,7 +27,7 @@ def upgrade(container: AsyncContainer) -> None:
     """Upgrade."""
 
     async def _fix_krbadmin_role(connection: AsyncConnection) -> None:  # noqa: ARG001
-        async with container() as cnt:
+        async with container(scope=Scope.REQUEST) as cnt:
             session = await cnt.get(AsyncSession)
             role_dao = await cnt.get(RoleDAO)
             role_use_case = await cnt.get(RoleUseCase)
