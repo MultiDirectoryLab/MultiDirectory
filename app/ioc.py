6d09e473202e67bb595dda72be495069deeb7ced
iyashnov <57270538+iyashnov@users.noreply.github.com>
2026-03-04 13:18:50 +0300

    Feature: Bind9 to PowerDNS migration (#954)

--- app/ioc.py
+++ app/ioc.py
@@ -65,6 +65,9 @@ from ldap_protocol.dns import (
     RemoteDNSManager,
     StubDNSManager,
 )
+from ldap_protocol.dns.bind_to_pdns_migration_use_case import (
+    BindToPDNSMigrationUseCase,
+)
 from ldap_protocol.identity import IdentityProvider
 from ldap_protocol.identity.provider_gateway import IdentityProviderGateway
 from ldap_protocol.kerberos import AbstractKadmin, get_kerberos_class
@@ -333,6 +336,25 @@ class MainProvider(Provider):
             yield RemoteDNSManager(settings=dns_settings)
         else:
             yield StubDNSManager(settings=dns_settings)
+
+    @provide(scope=Scope.REQUEST)
+    async def get_dns_migration_usecase(
+        self,
+        dns_settings: DNSSettingsDTO,
+        power_dns_auth_client: PowerDNSAuthHTTPClient,
+        power_dns_recursor_client: PowerDNSRecursorHTTPClient,
+        power_dns_dist_client: PowerDNSDistClient,
+    ) -> AsyncIterator[BindToPDNSMigrationUseCase]:
+        """Get DNS migration manager class."""
+        yield BindToPDNSMigrationUseCase(
+            PowerDNSManager(
+                settings=dns_settings,
+                power_dns_auth_client=power_dns_auth_client,
+                power_dns_recursor_client=power_dns_recursor_client,
+                dnsdist_client=power_dns_dist_client,
+            ),
+            dns_settings=dns_settings,
+        )
 
     @provide(scope=Scope.APP)
     async def get_redis_for_sessions(
