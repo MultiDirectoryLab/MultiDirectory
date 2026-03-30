370dc13f33c2fcd1baa45b86c10dc74556219762
m.shvets <m89258262994@yandex.com>
2026-03-30 13:10:37 +0300

    Update down_revision in Alembic migration to reflect new dependency

--- app/alembic/versions/552b4eafb1aa_remove_objectsid_vals.py
+++ app/alembic/versions/552b4eafb1aa_remove_objectsid_vals.py
@@ -38,7 +38,7 @@ from repo.pg.tables import queryable_attr as qa
 
 # revision identifiers, used by Alembic.
 revision: None | str = "552b4eafb1aa"
-down_revision: None | str = "19d86e660cf2"
+down_revision: None | str = "df4287898910"
 branch_labels: None | list[str] = None
 depends_on: None | list[str] = None
 
